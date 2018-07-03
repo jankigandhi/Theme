1ba9ab9ec0e405b6fc0aac1d42d4a6d3ebde08b7
Harsh Sharma <jankigandhi@atyantik.com>
2018-07-03 16:07:23 +0530

    button design changes

--- components/Buttons/button.php
+++ components/Buttons/button.php
@@ -3,65 +3,131 @@
 </head>
 <body>
 <div class="container">
-  <div class="py-4">
-    <h3>Basic</h3>
-    <div>
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
-      <button type="button" class="btn btn-link">Link</button>
+    <div class="py-4">
+        <h3>Basic</h3>
+        <div>
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <button type="button" class="btn btn-success">Success</button>
+            <button type="button" class="btn btn-danger">Danger</button>
+            <button type="button" class="btn btn-warning">Warning</button>
+            <button type="button" class="btn btn-info">Info</button>
+            <button type="button" class="btn btn-light">Light</button>
+            <button type="button" class="btn btn-dark">Dark</button>
+            <button type="button" class="btn btn-link">Link</button>
+        </div>
     </div>
-  </div>
-  <div class="py-4">
-    <h3>Style-1</h3>
-    <div>
-      <button type="button" class="btn-thick rounded-0 btn btn-outline-primary">Primary</button>
-      <button type="button" class="btn-slide btn-thick rounded-0 btn btn-outline-secondary">Secondary</button>
-      <button type="button" class="btn-slide slide-1 btn-thick rounded-0 btn btn-outline-success">Success</button>
-      <button type="button" class="btn-slide slide-2 btn-thick rounded-0 btn btn-outline-danger">Danger</button>
-      <button type="button" class="btn-slide slide-3 btn-thick rounded-0 btn btn-outline-warning">Warning</button>
-      <button type="button" class="btn-slide slide-4 btn-thick rounded-0 btn btn-outline-info">Info</button>
-      <button type="button" class="btn-slide slide-5 btn-thick rounded-0 btn btn-outline-dark">Dark</button>
+    <div class="py-4">
+        <h3>Style-1</h3>
+        <div>
+            <button type="button" class="btn-thick rounded-0 btn btn-outline-primary">Primary</button>
+            <button type="button" class="btn-slide btn-thick rounded-0 btn btn-outline-secondary">Secondary</button>
+            <button type="button" class="btn-slide slide-1 btn-thick rounded-0 btn btn-outline-success">Success</button>
+            <button type="button" class="btn-slide slide-2 btn-thick rounded-0 btn btn-outline-danger">Danger</button>
+            <button type="button" class="btn-slide slide-3 btn-thick rounded-0 btn btn-outline-warning">Warning</button>
+            <button type="button" class="btn-slide slide-4 btn-thick rounded-0 btn btn-outline-info">Info</button>
+            <button type="button" class="btn-slide slide-5 btn-thick rounded-0 btn btn-outline-dark">Dark</button>
+        </div>
     </div>
-  </div>
-  <div class="py-4">
-    <h3>Style-2</h3>
-    <div>
-      <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-primary">
-        <i class="btn-icon zmdi zmdi-shopping-cart"></i>
-        Primary
-      </button>
-      <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-secondary">
-        <i class="btn-icon zmdi zmdi-shopping-cart-plus"></i>
-        Secondary
-      </button>
-      <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-success">
-        <i class="btn-icon zmdi zmdi-search"></i>
-        Success
-      </button>
-      <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-danger">
-        <i class="btn-icon zmdi zmdi-mail-send"></i>
-        Danger
-      </button>
-      <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-warning">
-        <i class="btn-icon zmdi zmdi-email-open"></i>
-        Warning
-      </button>
-      <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-info">
-        <i class="btn-icon zmdi zmdi-favorite"></i>
-        Info
-      </button>
-      <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-dark">
-        <i class="btn-icon zmdi zmdi-edit"></i>
-        Dark
-      </button>
+    <div class="py-4">
+        <h3>Style-2</h3>
+        <div>
+            <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-primary">
+            <i class="btn-icon zmdi zmdi-shopping-cart"></i>
+            Primary
+            </button>
+            <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-secondary">
+            <i class="btn-icon zmdi zmdi-shopping-cart-plus"></i>
+            Secondary
+            </button>
+            <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-success">
+            <i class="btn-icon zmdi zmdi-search"></i>
+            Success
+            </button>
+            <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-danger">
+            <i class="btn-icon zmdi zmdi-mail-send"></i>
+            Danger
+            </button>
+            <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-warning">
+            <i class="btn-icon zmdi zmdi-email-open"></i>
+            Warning
+            </button>
+            <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-info">
+            <i class="btn-icon zmdi zmdi-favorite"></i>
+            Info
+            </button>
+            <button type="button" class="btn-with-icon btn-thick rounded-0 btn btn-dark">
+            <i class="btn-icon zmdi zmdi-edit"></i>
+            Dark
+            </button>
+        </div>
     </div>
-  </div>
+    <div class="py-4">
+        <h3>Style-3</h3>
+        <div>
+            <button type="button" class="top-hover btn-thick rounded-0 btn btn-primary">
+                <span class="hover-text hover-top text-uppercase">Add To Cart</span>
+                <span><i class="icon icon-1 zmdi zmdi-shopping-cart"></i></span> 
+            </button>
+            <button type="button" class="top-hover btn-thick rounded-0 btn btn-secondary">
+                <span class="hover-text hover-top text-uppercase">Delete</span>
+                <span><i class="icon icon-1 zmdi zmdi-delete"></i></i></span> 
+            </button>
+            <button type="button" class="top-hover btn-thick rounded-0 btn btn-success">
+                <span class="hover-text hover-top text-uppercase">Setting</span>
+                <span><i class="icon icon-1 zmdi zmdi-settings"></i></i></span> 
+            </button>
+            <button type="button" class="top-hover btn-thick rounded-0 btn btn-danger">
+                <span class="hover-text left-hover text-uppercase">Add To Cart</span>
+                <span><i class="icon icon-2 icon-left zmdi zmdi-shopping-cart"></i></i></span> 
+            </button>
+            <button type="button" class="top-hover btn-thick rounded-0 btn btn-warning">
+                <span class="hover-text left-hover  text-uppercase">Delete</span>
+                <span><i class="icon icon-2 zmdi zmdi-delete"></i></i></span> 
+            </button>
+            <button type="button" class="top-hover btn-thick rounded-0 btn btn-info">
+                <span class="hover-text left-hover text-uppercase">Setting</span>
+                <span><i class="icon icon-2 zmdi zmdi-settings"></i></i></span> 
+            </button>
+            <button type="button" class="top-hover btn-thick rounded-0 btn btn-dark">
+                <span class="hover-text left-hover text-uppercase">Setting</span>
+                <span><i class="icon icon-2 zmdi zmdi-settings"></i></i></span> 
+            </button>
+        </div>
+    </div>
+    <div class="py-4">
+        <h3>Style-4</h3>
+        <div>
+            <button type="button" class="btn-rounded btn-arrows btn-thick btn btn-outline-primary px-5">
+                <span class="">Primary</span>
+                <span><i class="arrow-icon arrow-1 zmdi zmdi-long-arrow-right"></i></i></span> 
+            </button>
+            <button type="button" class="btn-rounded btn-arrows btn-thick btn btn-outline-secondary px-5">
+                <span><i class="arrow-icon arrow-2 zmdi zmdi-long-arrow-left"></i></i></span> 
+                <span class="">secondary</span>
+            </button>
+            <button type="button" class="btn-rounded btn-arrows btn-thick btn btn-outline-success px-5">
+                <span class="">success</span>
+                <span><i class="arrow-icon arrow-3 zmdi zmdi-long-arrow-right"></i></i></span> 
+            </button>
+            <button type="button" class="btn-rounded btn-arrows btn-thick btn btn-outline-danger px-5">
+                <span><i class="arrow-icon arrow-4 zmdi zmdi-long-arrow-left"></i></i></span>
+                <span class="">success</span>
+            </button>
+            <button type="button" class="btn-rounded btn-arrows btn-thick btn btn-outline-warning px-5">
+                 <span class="">warning</span>
+                <span><i class="arrow-icon arrow-1 zmdi zmdi-long-arrow-right"></i></span>
+            </button>
+            <button type="button" class="btn-rounded btn-arrows btn-thick btn btn-outline-info px-5">
+                <span><i class="arrow-icon arrow-2 zmdi zmdi-long-arrow-left"></i></span> 
+                <span class="">info</span>
+            </button>
+            <button type="button" class="btn-rounded btn-arrows btn-thick btn btn-outline-dark px-5">
+                <span class="">dark</span>
+                <span><i class="arrow-icon arrow-3 zmdi zmdi-long-arrow-right"></i></span> 
+            </button>
+        </div>
+    </div>
 </div>
 </body>
 </html>
