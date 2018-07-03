c6b791978a2f2d3b87b5fdb5fe500ee2cb90c74c
heli shah <helishah@atyantik.com>
2018-07-03 12:46:56 +0530

    designing card components

--- components/card/card.php
+++ components/card/card.php
@@ -4,31 +4,210 @@
 <body>
 <div class="container my-4">
     <!-- Card -->
-    <div class="row mx-0">
-        <div class="col-lg-4 col-md-12 mb-4">
-            <div class="card card-cascade narrower">
-                <div class="view view-cascade gradient-card-header purple-gradient text-center text-white">
-                    <h2 class="card-header-title font-weight-bold">Ally Cooks</h2>
-                    <h5 class="mb-0 pb-3 pt-2">Follow me</h5>
-                    <div>
-                        <a class="btn btn-floating">
-                            <i class="zmdi zmdi-facebook"></i>
+    <div class="card-section card-cascade wider reverse card border-0">
+        <!-- Card image -->
+        <div class="view view-cascade overlay">
+            <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Card image cap">
+            <a href="#" class="img-link">
+                <div class="mask rgba-white-slight w-100 h-100"></div>
+            </a>
+        </div>
+        <!-- Card content -->
+        <div class="card-body card-body-cascade text-center bg-white">
+            <!-- Title -->
+            <h4 class="card-title"><strong>My adventure</strong></h4>
+            <!-- Subtitle -->
+            <h6 class="indigo-text font-weight-bold py-2">Photography</h6>
+            <!-- Text -->
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem perspiciatis voluptatum a, quo nobis, non commodi quia repellendus sequi nulla voluptatem dicta reprehenderit, placeat laborum ut beatae ullam suscipit veniam.
+            </p>
+            <!-- Linkedin -->
+            <a class="social-icon li-ic px-2"><i class="zmdi zmdi-linkedin"></i></a>
+            <!-- Twitter -->
+            <a class="social-icon tw-ic px-2"><i class="zmdi zmdi-twitter"></i></a>
+            <!-- Dribbble -->
+            <a class="social-icon fb-ic px-2"><i class="zmdi zmdi-facebook"></i></a>
+        </div>
+    </div>
+    <!-- Card -->
+    <div  class="py-4">
+        <h3 class="text-center">Action Button using card layout</h3>
+        <div class="row mx-0">
+            <div class="col-md-4 py-2">
+                <div class="card-section card">
+                    <!-- Card image -->
+                    <div class="view overlay">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/food.jpg" alt="Card image cap">
+                        <a>
+                            <div class="mask rgba-white-slight"></div>
                         </a>
-                        <a class="btn btn-floating">
-                            <i class="zmdi zmdi-email"></i>
+                    </div>
+                    <!-- Button -->
+                    <a class="btn-floating btn-action mdb-color lighten-3 ml-auto mr-4 align-middle d-inline-flex align-items-center justify-content-center m-2 p-0 text-white text-center"><i class="right-arrow zmdi zmdi-chevron-right pl-1"></i></a>
+                    <!-- Card content -->
+                    <div class="card-body">
+                        <!-- Title -->
+                        <h4 class="card-title font-weight-bold">Card title</h4>
+                        <hr>
+                        <!-- Text -->
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <!-- Card footer -->
+                    <div class="rounded-bottom mdb-color lighten-3 text-center pt-3 text-white">
+                        <ul class="list-unstyled list-inline font-small">
+                            <li class="list-inline-item pr-2 white-text"><i class="zmdi zmdi-clock-o pr-1"></i>05/10/2015</li>
+                            <li class="list-inline-item pr-2"><a href="#" class="text-white"><i class="zmdi zmdi-email pr-1 text-white"></i>12</a></li>
+                            <li class="list-inline-item pr-2"><a href="#" class="text-white"><i class="zmdi zmdi-facebook pr-1"> </i>21</a></li>
+                            <li class="list-inline-item"><a href="#" class="text-white"><i class="zmdi zmdi-twitter pr-1"> </i>5</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 py-2">
+                <div class="card-section card">
+                    <!-- Card image -->
+                    <div class="view overlay">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/food.jpg" alt="Card image cap">
+                        <a>
+                            <div class="mask rgba-white-slight"></div>
                         </a>
-                        <a class="btn btn-floating">
-                            <i class="zmdi zmdi-twitter"></i>
+                    </div>
+                    <!-- Button -->
+                    <a class="btn-floating btn-action mdb-color lighten-3 ml-auto mr-4 align-middle d-inline-flex align-items-center justify-content-center m-2 p-0 text-white text-center"><i class="right-arrow zmdi zmdi-chevron-right pl-1"></i></a>
+                    <!-- Card content -->
+                    <div class="card-body">
+                        <!-- Title -->
+                        <h4 class="card-title font-weight-bold">Card title</h4>
+                        <hr>
+                        <!-- Text -->
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <!-- Card footer -->
+                    <div class="rounded-bottom mdb-color lighten-3 text-center pt-3 text-white">
+                        <ul class="list-unstyled list-inline font-small">
+                            <li class="list-inline-item pr-2 white-text"><i class="zmdi zmdi-clock-o pr-1"></i>05/10/2015</li>
+                            <li class="list-inline-item pr-2"><a href="#" class="text-white"><i class="zmdi zmdi-email pr-1 text-white"></i>12</a></li>
+                            <li class="list-inline-item pr-2"><a href="#" class="text-white"><i class="zmdi zmdi-facebook pr-1"> </i>21</a></li>
+                            <li class="list-inline-item"><a href="#" class="text-white"><i class="zmdi zmdi-twitter pr-1"> </i>5</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 py-2">
+                <div class="card-section card">
+                    <!-- Card image -->
+                    <div class="view overlay">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/food.jpg" alt="Card image cap">
+                        <a>
+                            <div class="mask rgba-white-slight"></div>
                         </a>
+                    </div>
+                    <!-- Button -->
+                    <a class="btn-floating btn-action mdb-color lighten-3 ml-auto mr-4 align-middle d-inline-flex align-items-center justify-content-center m-2 p-0 text-white text-center"><i class="right-arrow zmdi zmdi-chevron-right pl-1"></i></a>
+                    <!-- Card content -->
+                    <div class="card-body">
+                        <!-- Title -->
+                        <h4 class="card-title font-weight-bold">Card title</h4>
+                        <hr>
+                        <!-- Text -->
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <!-- Card footer -->
+                    <div class="rounded-bottom mdb-color lighten-3 text-center pt-3 text-white">
+                        <ul class="list-unstyled list-inline font-small">
+                            <li class="list-inline-item pr-2 white-text"><i class="zmdi zmdi-clock-o pr-1"></i>05/10/2015</li>
+                            <li class="list-inline-item pr-2"><a href="#" class="text-white"><i class="zmdi zmdi-email pr-1 text-white"></i>12</a></li>
+                            <li class="list-inline-item pr-2"><a href="#" class="text-white"><i class="zmdi zmdi-facebook pr-1"> </i>21</a></li>
+                            <li class="list-inline-item"><a href="#" class="text-white"><i class="zmdi zmdi-twitter pr-1"> </i>5</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 col-md-12 mb-4">
-            
-        </div>
-        <div class="col-lg-4 col-md-12 mb-4">
-            
+    </div>
+    <div class="pb-4">
+        <div class="row mx-0">
+            <div class="col-md-4 py-2">
+                <div class="card-section card-cascade wider reverse card border-0">
+                    <!-- Card image -->
+                    <div class="view view-cascade overlay">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Card image cap">
+                        <a href="#" class="img-link">
+                            <div class="mask rgba-white-slight w-100 h-100"></div>
+                        </a>
+                    </div>
+                    <!-- Card content -->
+                    <div class="card-body card-body-cascade text-center bg-white">
+                        <!-- Title -->
+                        <h4 class="card-title"><strong>My adventure</strong></h4>
+                        <!-- Subtitle -->
+                        <h6 class="indigo-text font-weight-bold py-2">Photography</h6>
+                        <!-- Text -->
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem perspiciatis voluptatum a, quo nobis, non commodi quia repellendus sequi nulla voluptatem dicta reprehenderit, placeat laborum ut beatae ullam suscipit veniam.
+                        </p>
+                        <!-- Linkedin -->
+                        <a class="social-icon li-ic px-2"><i class="zmdi zmdi-linkedin"></i></a>
+                        <!-- Twitter -->
+                        <a class="social-icon tw-ic px-2"><i class="zmdi zmdi-twitter"></i></a>
+                        <!-- Dribbble -->
+                        <a class="social-icon fb-ic px-2"><i class="zmdi zmdi-facebook"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 py-2">
+                <div class="card-section card-cascade wider reverse card border-0">
+                    <!-- Card image -->
+                    <div class="view view-cascade overlay">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Card image cap">
+                        <a href="#" class="img-link">
+                            <div class="mask rgba-white-slight w-100 h-100"></div>
+                        </a>
+                    </div>
+                    <!-- Card content -->
+                    <div class="card-body card-body-cascade text-center bg-white">
+                        <!-- Title -->
+                        <h4 class="card-title"><strong>My adventure</strong></h4>
+                        <!-- Subtitle -->
+                        <h6 class="indigo-text font-weight-bold py-2">Photography</h6>
+                        <!-- Text -->
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem perspiciatis voluptatum a, quo nobis, non commodi quia repellendus sequi nulla voluptatem dicta reprehenderit, placeat laborum ut beatae ullam suscipit veniam.
+                        </p>
+                        <!-- Linkedin -->
+                        <a class="social-icon li-ic px-2"><i class="zmdi zmdi-linkedin"></i></a>
+                        <!-- Twitter -->
+                        <a class="social-icon tw-ic px-2"><i class="zmdi zmdi-twitter"></i></a>
+                        <!-- Dribbble -->
+                        <a class="social-icon fb-ic px-2"><i class="zmdi zmdi-facebook"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 py-2">
+                <div class="card-section card-cascade wider reverse card border-0">
+                    <!-- Card image -->
+                    <div class="view view-cascade overlay">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Card image cap">
+                        <a href="#" class="img-link">
+                            <div class="mask rgba-white-slight w-100 h-100"></div>
+                        </a>
+                    </div>
+                    <!-- Card content -->
+                    <div class="card-body card-body-cascade text-center bg-white">
+                        <!-- Title -->
+                        <h4 class="card-title"><strong>My adventure</strong></h4>
+                        <!-- Subtitle -->
+                        <h6 class="indigo-text font-weight-bold py-2">Photography</h6>
+                        <!-- Text -->
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem perspiciatis voluptatum a, quo nobis, non commodi quia repellendus sequi nulla voluptatem dicta reprehenderit, placeat laborum ut beatae ullam suscipit veniam.
+                        </p>
+                        <!-- Linkedin -->
+                        <a class="social-icon li-ic px-2"><i class="zmdi zmdi-linkedin"></i></a>
+                        <!-- Twitter -->
+                        <a class="social-icon tw-ic px-2"><i class="zmdi zmdi-twitter"></i></a>
+                        <!-- Dribbble -->
+                        <a class="social-icon fb-ic px-2"><i class="zmdi zmdi-facebook"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
