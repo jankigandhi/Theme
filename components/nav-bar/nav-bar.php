adfc9ab84519b5f5b6455a2caa3977ace64c79ca
Harsh Sharma <jankigandhi@atyantik.com>
2018-07-04 11:12:47 +0530

    design changes

---
+++ components/nav-bar/nav-bar.php
@@ -0,0 +1,40 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" type="text/css" href="css/nav-bar.css">
+</head>
+<body>
+<header>
+	<div class="site-header w-100 d-inline-block">
+		<div class="header-menu">
+			<h1 class="site-logo float-left">
+			<a href="">Logo</a>
+			</h1>
+			<div class="site-link">
+				<ul class="menu-bar list-unstyled top">
+					<li>
+						<a href="">Home</a>
+					</li>
+					<li>
+						<a href="">About</a>
+					</li>
+					<li>
+						<a href="">Service</a>
+					</li>
+					<li>
+						<a href="">Help</a>
+					</li>
+					<li>
+						<a href="">Contact</a>
+					</li>
+					<li>
+						<a href="">Carrer</a>
+					</li>
+				</ul>
+			</div>
+			<div class="bar-icon">
+				<a href="">☰</a>
+			</div>
+		</div>
+	</div>
+</header>
+</body>
+</html>
