ee16d3385240c81772078bd3a7c6bc5ae1b1cbe7
Harsh Sharma <jankigandhi@atyantik.com>
2018-07-04 18:41:14 +0530

    design changes

--- components/nav-bar/nav-bar.php
+++ components/nav-bar/nav-bar.php
@@ -2,39 +2,87 @@
 <link rel="stylesheet" type="text/css" href="css/nav-bar.css">
 </head>
 <body>
-<header>
-	<div class="site-header w-100 d-inline-block">
-		<div class="header-menu">
-			<h1 class="site-logo float-left">
-			<a href="">Logo</a>
-			</h1>
-			<div class="site-link">
-				<ul class="menu-bar list-unstyled top">
-					<li>
-						<a href="">Home</a>
-					</li>
-					<li>
-						<a href="">About</a>
-					</li>
-					<li>
-						<a href="">Service</a>
-					</li>
-					<li>
-						<a href="">Help</a>
-					</li>
-					<li>
-						<a href="">Contact</a>
-					</li>
-					<li>
-						<a href="">Carrer</a>
-					</li>
-				</ul>
-			</div>
-			<div class="bar-icon">
-				<a href="">☰</a>
+<!-- <div>
+	<div class="header-section">
+		<div class="site-header w-100 header-transperent my-3 px-4">
+			<div class="header-menu">
+				<h1 class="site-logo mb-0 float-left">
+				<a href="">Logo</a>
+				</h1>
+				<div class="site-link float-right">
+					<ul class="menu-bar top list-unstyled mb-0">
+						<li class="p-3">
+							<a href="">Home</a>
+						</li>
+						<li class="p-3">
+							<a href="">About</a>
+						</li>
+						<li class="p-3">
+							<a href="">Service</a>
+						</li>
+						<li class="p-3">
+							<a href="">Help</a>
+						</li>
+						<li class="p-3">
+							<a href="">Contact</a>
+						</li>
+						<li class="p-3">
+							<a href="">Carrer</a>
+						</li>
+					</ul>
+				</div>
+				<div class="bar-icon">
+					<a href="">☰</a>
+				</div>
 			</div>
 		</div>
 	</div>
-</header>
+	<div>
+		<div class="banner-image" style="background-image: url('images/banner-img.jpg');">
+			<h1 class="transforam-text">Transforam</h1>
+		</div>
+	</div>
+</div> -->
+<div>
+	<div class="line-height">
+		<div class="site-header header-transperent w-100">
+			<div class="header-menu">
+				<h1 class="site-logo mb-0 mt-1 float-left">
+				<a href="">Logo</a>
+				</h1>
+				<div class="site-link float-right">
+					<ul class="menu-bar top list-unstyled mb-0">
+						<li class="p-3">
+							<a href="">Home</a>
+						</li>
+						<li class="p-3">
+							<a href="">About</a>
+						</li>
+						<li class="p-3">
+							<a href="">Service</a>
+						</li>
+						<li class="p-3">
+							<a href="">Help</a>
+						</li>
+						<li class="p-3">
+							<a href="">Contact</a>
+						</li>
+						<li class="p-3">
+							<a href="">Carrer</a>
+						</li>
+					</ul>
+				</div>
+				<div class="bar-icon mt-2">
+					<a href="">☰</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div class="banner-image" style="background-image: url('images/banner-img.jpg');">
+			<h1 class="transforam-text">Transform</h1>
+		</div>
+	</div>
+</div>
 </body>
 </html>
