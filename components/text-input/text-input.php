f2795bcadb73f8a21f1b7e7bf8c7c2c27c874ac2
heli shah <helishah@atyantik.com>
2018-07-04 18:26:41 +0530

    designing text-input  components

---
+++ components/text-input/text-input.php
@@ -0,0 +1,190 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" type="text/css" href="css/text-input.css">
+</head>
+<body>
+<div class="container my-4">
+    <div class="mb-5">
+        <h3>style-1</h3>
+        <form>
+            <div class="custom-style-1 row mx-0">
+                <div class="col-md-6 py-2">
+                    <div class="my-4">
+                        <div class="custom-input form-group mb-4">
+                            <input type="name" class="input-effect transparent-box pl-0 form-control rounded-0 " placeholder=" " />
+                            <span class="bar d-block w-100"></span>
+                            <label class="text-uppercase text-left w-100">First Name</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 py-2">
+                    <div class="my-4">
+                        <div class="custom-input form-group mb-4">
+                            <input type="name" class="input-effect transparent-box pl-0 form-control rounded-0" placeholder=" " />
+                            <span class="bar d-block w-100"></span>
+                            <label class="text-uppercase text-left w-100">Last Name</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 py-2">
+                    <div class="my-4">
+                        <div class="custom-input form-group mb-4">
+                            <input type="email" class="input-effect transparent-box pl-0 form-control rounded-0" placeholder=" " />
+                            <span class="bar d-block w-100"></span>
+                            <label class="text-uppercase text-left w-100">Email</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 py-2">
+                    <div class="my-4">
+                        <div class="custom-input form-group mb-4">
+                            <input type="text" class="input-effect transparent-box pl-0 form-control rounded-0" placeholder=" " />
+                            <span class="bar d-block w-100"></span>
+                            <label class="text-uppercase text-left w-100">Password</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="mb-5">
+        <h3>Style-2</h3>
+        <div class="row mx-0">
+            <div class="col-md-6 py-2">
+                <div class="custom-style-2 my-4">
+                    <div class="custom-input-2 form-group mb-4">
+                        <input type="text" class="input-effect-2 transparent-box-2 pl-2 form-control rounded-0" placeholder=" " />
+                        <label class="text-uppercase text-left w-100 pl-2">Frist Name</label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 py-2">
+                <div class="custom-style-2 my-4">
+                    <div class="custom-input-2 form-group mb-4">
+                        <input type="text" class="input-effect-2 transparent-box-2 pl-2 form-control rounded-0" placeholder=" " />
+                        <label class="text-uppercase text-left w-100 pl-2">Last Name</label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 py-2">
+                <div class="custom-style-2 my-4">
+                    <div class="custom-input-2 form-group mb-4">
+                        <input type="email" class="input-effect-2 transparent-box-2 pl-2 form-control rounded-0" placeholder=" " />
+                        <label class="text-uppercase text-left w-100 pl-2">Email</label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 py-2">
+                <div class="custom-style-2 my-4">
+                    <div class="custom-input-2 form-group mb-4">
+                        <input type="text" class="input-effect-2 transparent-box-2 pl-2 form-control rounded-0" placeholder=" " />
+                        <label class="text-uppercase text-left w-100 pl-2">Password</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mb-5">
+        <h3>Style-3</h3>
+        <form>
+            <div class="row mx-0">
+                <div class="col-md-6 py-2">
+                    <div class="custom-style-3  my-4">
+                        <div class="custom-input-3 form-group mb-4">
+                            <input type="name" class="input-effect-3 transparent-box-3 pl-0 form-control rounded-0" placeholder=" " />
+                            <label class="text-uppercase text-left w-100">First Name
+                                <span><i class="icon zmdi zmdi-edit pl-5"></i></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 py-2">
+                    <div class="custom-style-3  my-4">
+                        <div class="custom-input-3 form-group mb-4">
+                            <input type="name" class="input-effect-3 transparent-box-3 pl-0 form-control rounded-0" placeholder=" " />
+                            <label class="text-uppercase text-left w-100">Last Name
+                                <span><i class="icon zmdi zmdi-edit pl-5"></i></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 py-2">
+                    <div class="custom-style-3  my-4">
+                        <div class="custom-input-3 form-group mb-4">
+                            <input type="name" class="input-effect-3 transparent-box-3 pl-0 form-control rounded-0" placeholder=" " />
+                            <label class="text-uppercase text-left w-100">Email
+                                <span><i class="icon zmdi zmdi-edit pl-5"></i></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 py-2">
+                    <div class="custom-style-3  my-4">
+                        <div class="custom-input-3 form-group mb-4">
+                            <input type="name" class="input-effect-3 transparent-box-3 pl-0 form-control rounded-0" placeholder=" " />
+                            <label class="text-uppercase text-left w-100">Password
+                                <span><i class="icon zmdi zmdi-edit pl-5"></i></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="bg-light p-3 my-5">
+        <h3>Style-4</h3>
+        <form>
+            <div class="row mx-0">
+                <div class="col-md-6 py-2">
+                    <div class="custom-style-4 my-4">
+                        <div class="custom-input-4 form-group mb-4">
+                            <input type="name" class="input-effect-4 transparent-box-4 pl-5 form-control rounded-0" placeholder=" " />
+                            <label class="text-uppercase text-left w-100 pl-2">
+                                <span class="text-name">Time</span>
+                                <span><i class="icon zmdi zmdi-time"></i></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 py-2">
+                    <div class="custom-style-4 my-4">
+                        <div class="custom-input-4 form-group mb-4">
+                            <input type="name" class="input-effect-4 transparent-box-4 pl-5 form-control rounded-0" placeholder=" " />
+                            <label class="text-uppercase text-left w-100 pl-2"><span class="text-name">Phone</span>
+                            <span><i class="icon zmdi zmdi-phone"></i></span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+<div class="bg-light p-3 my-5">
+    <h3>Style-5</h3>
+    <form>
+        <div class="row mx-0">
+            <div class="col-md-6 py-2">
+                <div class="custom-style-4 my-4">
+                    <div class="custom-input-4 form-group mb-4">
+                        <input type="name" class="input-effect-4 transparent-box-5 pl-5 form-control border-radius-20" placeholder=" " />
+                        <label class="text-uppercase text-left w-100 pl-2">
+                            <span class="text-name">Time</span>
+                            <span><i class="icon zmdi zmdi-time"></i></span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 py-2">
+                <div class="custom-style-4 my-4">
+                    <div class="custom-input-4 form-group mb-4">
+                        <input type="name" class="input-effect-4 transparent-box-5 pl-5 form-control border-radius-20" placeholder=" " />
+                        <label class="text-uppercase text-left w-100 pl-2"><span class="text-name">Phone</span>
+                        <span><i class="icon zmdi zmdi-phone"></i></span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+</div>
+</body>
+</html>
