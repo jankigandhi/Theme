f7bfafaf1148258ca6e7d6a6e157d4ebe283bda9
heli shah <helishah@atyantik.com>
2018-07-05 17:51:46 +0530

    design changes in input-text components and add new breadcrumbs components

---
+++ components/breadcrumbs/breadcrumbs.php
@@ -0,0 +1,17 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" type="text/css" href="css/breadcrumbs.css">
+</head>
+<body>
+<div class="container my-4">
+    <div>
+        <ul class="custom-breadcrumb list-unstyled d-inline-block">
+            <li class="float-left bg-success"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-home"></i> </span></a></li>
+            <li class="float-left bg-success"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-tv"></i> </span> Projects</a></li>
+            <li class="float-left bg-success"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-label-alt-outline"></i></span> Breadcrumb</a></li>
+            <li class="float-left bg-success"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-star-circle"></i> </span> Getting started</a></li>
+            <li class="float-left bg-success"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-download"></i> </span> Download</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
