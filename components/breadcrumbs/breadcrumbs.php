51670e50ff344cb2dacfb7d49ed32be649808934
heli shah <helishah@atyantik.com>
2018-07-05 18:05:49 +0530

    design changes in input-text components and add new breadcrumbs components

--- components/breadcrumbs/breadcrumbs.php
+++ components/breadcrumbs/breadcrumbs.php
@@ -12,6 +12,42 @@
             <li class="float-left bg-success"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-download"></i> </span> Download</a></li>
         </ul>
     </div>
+    <div>
+        <ul class="custom-breadcrumb list-unstyled d-inline-block">
+            <li class="float-left bg-primary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-home"></i> </span></a></li>
+            <li class="float-left bg-primary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-tv"></i> </span> Projects</a></li>
+            <li class="float-left bg-primary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-label-alt-outline"></i></span> Breadcrumb</a></li>
+            <li class="float-left bg-primary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-star-circle"></i> </span> Getting started</a></li>
+            <li class="float-left bg-primary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-download"></i> </span> Download</a></li>
+        </ul>
+    </div>
+    <div>
+        <ul class="custom-breadcrumb list-unstyled d-inline-block">
+            <li class="float-left bg-secondary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-home"></i> </span></a></li>
+            <li class="float-left bg-secondary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-tv"></i> </span> Projects</a></li>
+            <li class="float-left bg-secondary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-label-alt-outline"></i></span> Breadcrumb</a></li>
+            <li class="float-left bg-secondary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-star-circle"></i> </span> Getting started</a></li>
+            <li class="float-left bg-secondary"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-download"></i> </span> Download</a></li>
+        </ul>
+    </div>
+    <div>
+        <ul class="custom-breadcrumb list-unstyled d-inline-block">
+            <li class="float-left bg-warning"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-home"></i> </span></a></li>
+            <li class="float-left bg-warning"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-tv"></i> </span> Projects</a></li>
+            <li class="float-left bg-warning"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-label-alt-outline"></i></span> Breadcrumb</a></li>
+            <li class="float-left bg-warning"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-star-circle"></i> </span> Getting started</a></li>
+            <li class="float-left bg-warning"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-download"></i> </span> Download</a></li>
+        </ul>
+    </div>
+    <div>
+        <ul class="custom-breadcrumb list-unstyled d-inline-block">
+            <li class="float-left bg-danger"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-home"></i> </span></a></li>
+            <li class="float-left bg-danger"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-tv"></i> </span> Projects</a></li>
+            <li class="float-left bg-danger"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-label-alt-outline"></i></span> Breadcrumb</a></li>
+            <li class="float-left bg-danger"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-star-circle"></i> </span> Getting started</a></li>
+            <li class="float-left bg-danger"><a href="#" class="text-white d-block text-center mr-4"><span class="icon pr-1"><i class="zmdi zmdi-download"></i> </span> Download</a></li>
+        </ul>
+    </div>
 </div>
 </body>
 </html>
