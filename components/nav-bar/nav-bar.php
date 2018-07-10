720407e0d4f904526518cbb74baa3f23cb2f6c3a
Harsh Sharma <jankigandhi@atyantik.com>
2018-07-10 18:47:54 +0530

    design changes

--- components/nav-bar/nav-bar.php
+++ components/nav-bar/nav-bar.php
@@ -3,87 +3,380 @@
 <link rel="stylesheet" type="text/css" href="css/nav-bar.css">
 </head>
 <body>
-<div>
-	<div class="line-height">
+	<header class="line-height">
+		<!-- <div class="nav-utility w-100 d-inline-block px-4 py-2">
+			<div class="navigation float-left">
+				<i class="zmdi zmdi-gps-dot"></i>
+				<span>68 Cardamon Place, Melbourne Vic 3000</span>
+			</div>
+			<div class="mail float-left pl-4">
+				<i class="zmdi zmdi-email"></i>
+				<span>hello@foundry.net</span>
+			</div>
+			<div class="float-right">
+				<button class="btn btn-outline-success rounded-0 px-3 text-uppercase btn-sm">Try Builder</button>
+				<button class="btn btn-success rounded-0 px-3 text-uppercase btn-sm">Buy Now</button>
+			</div>
+		</div> -->
 		<div class="site-header header-transperent w-100">
 			<div class="header-menu">
-				<h1 class="site-logo mb-0 mt-1 float-left">
-				<a href="">Logo</a>
-				</h1>
+				<p class="site-logo mb-0 px-4  py-1 line-height float-left">
+					<a href="#" class="text-uppercase">Logo</a>
+				</p>
 				<div class="site-link float-right">
-					<ul class="menu-bar top list-unstyled mb-0">
-						<li class="p-3">
-							<a href="">Home</a>
-						</li>
-						<li class="p-3">
-							<a href="">About</a>
-						</li>
-						<li class="p-3">
-							<a href="">Service</a>
-						</li>
-						<li class="p-3">
-							<a href="">Help</a>
-						</li>
-						<li class="p-3">
-							<a href="">Contact</a>
-						</li>
-						<li class="p-3">
-							<a href="">Carrer</a>
-						</li>
-					</ul>
+					<div class="float-left">
+						<ul class="menu-bar top list-unstyled mb-0 px-5">
+							<li class="p-3">
+								<a href="#" class="text-uppercase">Home</a>
+								<ul class="sub-menu list-unstyled px-3">
+									<li>
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Concepts</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Adventure Travel</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Agency</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Agency 2</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">App Landing</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">App Landing 2</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Capital Firm</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Fashion</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Fitness</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Restaurant</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Music</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Event / Seminar</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Architecture</a>
+											</li>
+										</ul>
+									</li>
+									<li class="align-top">
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Photography</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Ken Burns</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Portfolio</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Property Listing</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Resumé</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Winery</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Home Pages</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Home Classic</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Home Layout 2</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Home Layout 3</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Home Layout 4</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Home Layout 5</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="p-3">
+								<a href="#" class="text-uppercase">Pages</a>
+								<ul class="sub-menu list-unstyled px-3">
+									<li>
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Inner pages</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">About Us 1</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">About Us 2</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">About Us 3</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Services 1</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Services 2</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Services 3</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Contact 1</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Contact 2</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Planner</a>
+											</li>
+										</ul>
+									</li>
+									<li class="align-top">
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Utility Pages</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Login</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Register</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Comning Soon</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Faq</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Confirmation</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Countdown</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Countdown 2</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">404 - Error</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="p-3">
+								<a href="#" class="text-uppercase">Elements</a>
+								<ul class="sub-menu list-unstyled px-3">
+									<li>
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Elements</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Buttons</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Tabbed Content</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Progress Bars</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Accordions</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Alerts</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Form Inputs</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Countdowns</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Modals</a>
+											</li>
+										</ul>
+									</li>
+									<li class="align-top">
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Elements</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Pricing Tables</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Icon Boxes</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Widgets</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Typography</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Lists</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Grid Structure</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Text &amp; Image</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Intro Sections</a>
+											</li>
+										</ul>
+									</li>
+									<li class="align-top">
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Elements</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Titles Left</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Titles Center</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Video Inline</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Video BG</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Video Slider</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Image Galleries</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Parallax</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase"> Nav Styles</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="p-3">
+								<a href="#" class="text-uppercase">Portfolio</a>
+								<ul class="sub-menu list-unstyled px-3">
+									<li>
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Contained</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Wide</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Parallax</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Single</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="p-3">
+								<a href="#" class="text-uppercase">Shop</a>
+								<ul class="sub-menu list-unstyled px-3">
+									<li>
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Shop Layout</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Product Single</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Cart</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Checkout</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="p-3">
+								<a href="#" class="text-uppercase">Blog</a>
+								<ul class="sub-menu list-unstyled px-3">
+									<li>
+										<ul class="list-unstyled">
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Listing</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Masonry</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Single</a>
+											</li>
+											<li class="p-2">
+												<a href="#" class="text-uppercase">Simple Feed</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<div class="float-left">
+						<div class="search-icon p-3">
+							<span class="search align-middle">
+								<i class="zmdi zmdi-search"></i>
+							</span>
+						</div>
+					</div>
+					<div class="float-left">
+						<div class="search-icon p-3">
+							<span class="search align-middle">
+								<i class="zmdi zmdi-shopping-cart"></i>
+							</span>
+						</div>
+					</div>
+					<div class="float-left">
+						<div class="search-icon p-3">
+							<a href="#" class="text-uppercase mb-0">Eng</a>
+						</div>
+					</div>
 				</div>
 				<div class="bar-icon mt-2">
-					<a href="">☰</a>
+					<a href="#">☰</a>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div>
+	</header>
+	<main>
 		<div class="banner-image" style="background-image: url('images/banner-img.jpg');">
 			<h1 class="transforam-text">Transform</h1>
 		</div>
-	</div>
-</div>
-<div>
-	<div class="line-height">
-		<div class="site-header bg-dark w-100">
-			<div class="header-menu">
-				<h1 class="site-logo mb-0 mt-1 float-left">
-				<a href="">Logo</a>
-				</h1>
-				<div class="site-link float-right">
-					<ul class="menu-bar top list-unstyled mb-0">
-						<li class="p-3">
-							<a href="">Home</a>
-						</li>
-						<li class="p-3">
-							<a href="">About</a>
-						</li>
-						<li class="p-3">
-							<a href="">Service</a>
-						</li>
-						<li class="p-3">
-							<a href="">Help</a>
-						</li>
-						<li class="p-3">
-							<a href="">Contact</a>
-						</li>
-						<li class="p-3">
-							<a href="">Carrer</a>
-						</li>
-					</ul>
-				</div>
-				<div class="bar-icon mt-2">
-					<a href="">☰</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div>
-		<div class="banner-image" style="background-image: url('images/banner-img.jpg');">
-			<h1 class="transforam-text">Transform</h1>
-		</div>
-	</div>
-</div>
+	</main>
 </body>
 </html>
