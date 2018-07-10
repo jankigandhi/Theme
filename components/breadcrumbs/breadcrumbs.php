c8bf1dd6cbb0efdb6a9f1f0183a8d1508f831226
heli shah <helishah@atyantik.com>
2018-07-10 15:46:49 +0530

    changes in breadcrumbs components

--- components/breadcrumbs/breadcrumbs.php
+++ components/breadcrumbs/breadcrumbs.php
@@ -4,11 +4,11 @@
 </head>
 <body>
 <div class="container my-5">
-    <ul class="ribbon list-unstyled d-inline-block mr-4 text-center bg-primary">
-        <li class="active d-inline-block mr-4 text-center">home</li>
-        <li class="d-inline-block mr-4 text-center bg-primary">about</li>
-        <li class="d-inline-block mr-4 text-center bg-primary">faq</li>
-        <li class="d-inline-block mr-4 text-center bg-primary">login</li>
+    <ul class="ribbon list-unstyled d-inline-block mr-4 text-center">
+        <li class="active d-inline-block mr-4 text-center text-white">home</li>
+        <li class="d-inline-block mr-4 text-center text-white">about</li>
+        <li class="d-inline-block mr-4 text-center text-white">faq</li>
+        <li class="d-inline-block mr-4 text-center text-white">login</li>
     </ul>
 </div>
 </body>
