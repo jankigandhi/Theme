9f3f31c1f8d379aca4c7edd797a48076952b90d5
Janki Gandhi <jankigandhi@atyantik.com>
2018-07-10 17:45:43 +0530

    designing pagination

---
+++ components/pagination/pagination-1.php
@@ -0,0 +1,156 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../typography/css/typography.css">
+<link rel="stylesheet" type="text/css" href="css/pagination.css">
+</head>
+<body>
+<div class="container my-4">
+    <div class="mb-5">
+        <h3 class="mb-3">Basic</h3>
+        <nav>
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="mb-5">
+        <h3 class="mb-3">Style-1</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <nav>
+                    <ul class="pagination pagination-1 pagination-success">
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded" href="#">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item px-2 active"><a class="page-link border-0 rounded" href="#">1</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded" href="#">2</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded" href="#">3</a></li>
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded" href="#">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul class="pagination pagination-1 pagination-danger">
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded" href="#">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item px-2 active"><a class="page-link border-0 rounded" href="#">1</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded" href="#">2</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded" href="#">3</a></li>
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded" href="#">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul class="pagination pagination-1 pagination-dark">
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded" href="#">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item px-2 active"><a class="page-link border-0 rounded" href="#">1</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded" href="#">2</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded" href="#">3</a></li>
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded" href="#">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-6">
+                <nav>
+                    <ul class="pagination pagination-1 pagination-success">
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded-circle" href="#">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item px-2 active"><a class="page-link border-0 rounded-circle" href="#">1</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded-circle" href="#">2</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded-circle" href="#">3</a></li>
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded-circle" href="#">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul class="pagination pagination-1 pagination-danger">
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded-circle" href="#">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item px-2 active"><a class="page-link border-0 rounded-circle" href="#">1</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded-circle" href="#">2</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded-circle" href="#">3</a></li>
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded-circle" href="#">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul class="pagination pagination-1 pagination-dark">
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded-circle" href="#">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item px-2 active"><a class="page-link border-0 rounded-circle" href="#">1</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded-circle" href="#">2</a></li>
+                        <li class="page-item px-2"><a class="page-link border-0 rounded-circle" href="#">3</a></li>
+                        <li class="page-item px-2">
+                            <a class="page-link border-0 rounded-circle" href="#">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+               
+            </div>
+        </div>
+        
+    </div>
+</div>
+</body>
+</html>
