d523a339205d29819e90ab83194a953a58a5b78e
Janki Gandhi <jankigandhi@atyantik.com>
2018-07-12 18:15:31 +0530

    designing breadcrumbs

--- components/breadcrumbs/breadcrumbs-1.php
+++ components/breadcrumbs/breadcrumbs-1.php
@@ -6,7 +6,7 @@
 <div class="container my-5">
   <h1 class="h2 text-center my-3">Breadcrumbs</h1>
   <div class="mb-3">
-    <h4>Default</h4>
+    <h4>Type-1</h4>
     <ul class="breadcrumb">
       <li class="breadcrumb-item"><a href="#">Home</a></li>
       <li class="breadcrumb-item"><a href="#">Library</a></li>
@@ -14,8 +14,32 @@
     </ul>
   </div>
   <div class="mb-3">
-    <h4>Style-1</h4>
-    <ul class="breadcrumb breadcrumb-type1">
+    <h4>Type-2</h4>
+    <ul class="breadcrumb breadcrumb-type2">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Library</a></li>
+      <li class="breadcrumb-item active">Data</li>
+    </ul>
+  </div>
+  <div class="mb-3">
+    <h4>Type-3</h4>
+    <ul class="breadcrumb breadcrumb-type2">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Library</a></li>
+      <li class="breadcrumb-item active">Data</li>
+    </ul>
+  </div>
+  <div class="mb-3">
+    <h4>Type-4</h4>
+    <ul class="breadcrumb breadcrumb-type3">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Library</a></li>
+      <li class="breadcrumb-item active">Data</li>
+    </ul>
+  </div>
+  <div class="mb-3">
+    <h4>Type-5</h4>
+    <ul class="breadcrumb breadcrumb-type4">
       <li class="breadcrumb-item"><a href="#">Home</a></li>
       <li class="breadcrumb-item"><a href="#">Library</a></li>
       <li class="breadcrumb-item active">Data</li>
