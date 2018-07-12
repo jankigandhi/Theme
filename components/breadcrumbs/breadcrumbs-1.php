f33ebae3be6d27db832db209ca8a6ac893fbec7c
Janki Gandhi <jankigandhi@atyantik.com>
2018-07-12 17:27:10 +0530

    designing breadcrumbs

---
+++ components/breadcrumbs/breadcrumbs-1.php
@@ -0,0 +1,26 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../typography/css/typography.css">
+<link rel="stylesheet" type="text/css" href="css/breadcrumbs.css">
+</head>
+<body>
+<div class="container my-5">
+  <h1 class="h2 text-center my-3">Breadcrumbs</h1>
+  <div class="mb-3">
+    <h4>Default</h4>
+    <ul class="breadcrumb">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Library</a></li>
+      <li class="breadcrumb-item active">Data</li>
+    </ul>
+  </div>
+  <div class="mb-3">
+    <h4>Style-1</h4>
+    <ul class="breadcrumb breadcrumb-type1">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Library</a></li>
+      <li class="breadcrumb-item active">Data</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
