a49f041245b837804f8897d99e859493aac975ff
Harsh Sharma <jankigandhi@atyantik.com>
2018-07-12 17:08:26 +0530

    process-steps page design

---
+++ components/process-steps/process-steps.php
@@ -0,0 +1,112 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../typography/css/typography.css">
+<link rel="stylesheet" type="text/css" href="css/process-steps.css">
+</head>
+<body>
+<div class="container">
+	<h3 class="text-center my-4">Process Steps Variations</h3>
+	<ul class="list-unstyled process-primary process-steps process-steps-2 w-100 d-inline-block pb-3">
+		<li class="active steps text-center py-3">
+			<a href="#" class="rounded-box rounded-circle d-inline-block">
+				<span>1</span>
+			</a>
+			<h5 class="step-label mt-3">Review Cart</h5>
+		</li>
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded-circle d-inline-block">
+				<span>2</span>
+			</a>
+			<h5 class="step-label mt-3">Review Cart</h5>
+		</li>
+	</ul>
+	<ul class="list-unstyled process-success process-steps process-steps-3 w-100 d-inline-block pb-3">
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>1</span>
+			</a>
+			<h5 class="step-label mt-3">Review Cart</h5>
+		</li>
+		<li class="active steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>2</span>
+			</a>
+			<h5 class="step-label mt-3">Complete Payment</h5>
+		</li>
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>3</span>
+			</a>
+			<h5 class="step-label mt-3">Order Complete</h5>
+		</li>
+	</ul>
+	<ul class="list-unstyled process-danger process-steps process-steps-4 w-100 d-inline-block pb-3">
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded-circle d-inline-block">
+				<span>1</span>
+			</a>
+			<h5 class="step-label mt-3">Review Cart</h5>
+		</li>
+		<li class="active steps text-center py-3">
+			<a href="#" class="rounded-box rounded-circle d-inline-block">
+				<span>2</span>
+			</a>
+			<h5 class="step-label mt-3">Enter Shipping Info</h5>
+		</li>
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded-circle d-inline-block">
+				<span>3</span>
+			</a>
+			<h5 class="step-label mt-3">Complete Payment</h5>
+		</li>
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded-circle d-inline-block">
+				<span>4</span>
+			</a>
+			<h5 class="step-label mt-3">Order Complete</h5>
+		</li>
+	</ul>
+	<ul class="list-unstyled process-info process-steps process-steps-5 w-100 d-inline-block pb-3">
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>
+					<i class="zmdi zmdi-shopping-cart"></i>
+				</span>
+			</a>
+			<h5 class="step-label mt-3">Review Cart</h5>
+		</li>
+		<li class="active steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>
+					<i class="zmdi zmdi-edit"></i>
+				</span>
+			</a>
+			<h5 class="step-label mt-3">Enter Shipping Info</h5>
+		</li>
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>
+					<i class="zmdi zmdi-money-box"></i>
+				</span>
+			</a>
+			<h5 class="step-label mt-3">Enter Billing Info</h5>
+		</li>
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>
+					<i class="zmdi zmdi-thumb-up"></i>
+				</span>
+			</a>
+			<h5 class="step-label mt-3">Complete Payment</h5>
+		</li>
+		<li class="steps text-center py-3">
+			<a href="#" class="rounded-box rounded d-inline-block">
+				<span>
+					<i class="zmdi zmdi-check"></i>
+				</span>
+			</a>
+			<h5 class="step-label mt-3">Order Complete</h5>
+		</li>
+	</ul>
+</div>
+</body>
+</html>
