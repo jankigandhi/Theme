902bda28e248cd06cb94006a8ca663b5112320aa
heli shah <helishah@atyantik.com>
2018-07-13 16:54:31 +0530

    designing table components

--- components/table/table.php
+++ components/table/table.php
@@ -4,121 +4,119 @@
 </head>
 <body>
 <div class="container my-5">
-    <div class="p-5">
-        <div class="tbl-header table-responsive">
-            <table class="table-fixed table border-0">
-                <thead class="thead-dark">
-                    <tr>
-                        <th class="text-left text-uppercase font-weight-medium p-3">Code</th>
-                        <th class="text-left text-uppercase font-weight-medium p-3">Company</th>
-                        <th class="text-left text-uppercase font-weight-medium p-3">Price</th>
-                        <th class="text-left text-uppercase font-weight-medium p-3">Change</th>
-                        <th class="text-left text-uppercase font-weight-medium p-3">Change %</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-        <div class="tbl-content mt-0">
-            <table class="table border-0">
-                <tbody>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAC</td>
-                        <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
-                        <td class="text-left align-middle p-3 border-0">$1.38</td>
-                        <td class="text-left align-middle p-3 border-0">+2.01</td>
-                        <td class="text-left align-middle p-3 border-0">-0.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAD</td>
-                        <td class="text-left align-middle p-3 border-0">AUSENCO</td>
-                        <td class="text-left align-middle p-3 border-0">$2.38</td>
-                        <td class="text-left align-middle p-3 border-0">-0.01</td>
-                        <td class="text-left align-middle p-3 border-0">-1.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAX</td>
-                        <td class="text-left align-middle p-3 border-0">ADELAIDE</td>
-                        <td class="text-left align-middle p-3 border-0">$3.22</td>
-                        <td class="text-left align-middle p-3 border-0">+0.01</td>
-                        <td class="text-left align-middle p-3 border-0">+1.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">XXD</td>
-                        <td class="text-left align-middle p-3 border-0">ADITYA BIRLA</td>
-                        <td class="text-left align-middle p-3 border-0">$1.02</td>
-                        <td class="text-left align-middle p-3 border-0">-1.01</td>
-                        <td class="text-left align-middle p-3 border-0">+2.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAC</td>
-                        <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
-                        <td class="text-left align-middle p-3 border-0">$1.38</td>
-                        <td class="text-left align-middle p-3 border-0">+2.01</td>
-                        <td class="text-left align-middle p-3 border-0">-0.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAD</td>
-                        <td class="text-left align-middle p-3 border-0">AUSENCO</td>
-                        <td class="text-left align-middle p-3 border-0">$2.38</td>
-                        <td class="text-left align-middle p-3 border-0">-0.01</td>
-                        <td class="text-left align-middle p-3 border-0">-1.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAX</td>
-                        <td class="text-left align-middle p-3 border-0">ADELAIDE</td>
-                        <td class="text-left align-middle p-3 border-0">$3.22</td>
-                        <td class="text-left align-middle p-3 border-0">+0.01</td>
-                        <td class="text-left align-middle p-3 border-0">+1.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">XXD</td>
-                        <td class="text-left align-middle p-3 border-0">ADITYA BIRLA</td>
-                        <td class="text-left align-middle p-3 border-0">$1.02</td>
-                        <td class="text-left align-middle p-3 border-0">-1.01</td>
-                        <td class="text-left align-middle p-3 border-0">+2.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAC</td>
-                        <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
-                        <td class="text-left align-middle p-3 border-0">$1.38</td>
-                        <td class="text-left align-middle p-3 border-0">+2.01</td>
-                        <td class="text-left align-middle p-3 border-0">-0.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAD</td>
-                        <td class="text-left align-middle p-3 border-0">AUSENCO</td>
-                        <td class="text-left align-middle p-3 border-0">$2.38</td>
-                        <td class="text-left align-middle p-3 border-0">-0.01</td>
-                        <td class="text-left align-middle p-3 border-0">-1.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAX</td>
-                        <td class="text-left align-middle p-3 border-0">ADELAIDE</td>
-                        <td class="text-left align-middle p-3 border-0">$3.22</td>
-                        <td class="text-left align-middle p-3 border-0">+0.01</td>
-                        <td class="text-left align-middle p-3 border-0">+1.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">XXD</td>
-                        <td class="text-left align-middle p-3 border-0">ADITYA BIRLA</td>
-                        <td class="text-left align-middle p-3 border-0">$1.02</td>
-                        <td class="text-left align-middle p-3 border-0">-1.01</td>
-                        <td class="text-left align-middle p-3 border-0">+2.36%</td>
-                    </tr>
-                    <tr>
-                        <td class="text-left align-middle p-3 border-0">AAC</td>
-                        <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
-                        <td class="text-left align-middle p-3 border-0">$1.38</td>
-                        <td class="text-left align-middle p-3 border-0">+2.01</td>
-                        <td class="text-left align-middle p-3 border-0">-0.36%</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="tbl-header table-responsive">
+        <table class="table-fixed table border-0">
+            <thead class="thead-dark">
+                <tr>
+                    <th class="text-left text-uppercase font-weight-medium p-3">Code</th>
+                    <th class="text-left text-uppercase font-weight-medium p-3">Company</th>
+                    <th class="text-left text-uppercase font-weight-medium p-3">Price</th>
+                    <th class="text-left text-uppercase font-weight-medium p-3">Change</th>
+                    <th class="text-left text-uppercase font-weight-medium p-3">Change %</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    <div class="tbl-content mt-0">
+        <table class="table border-0">
+            <tbody>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAC</td>
+                    <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
+                    <td class="text-left align-middle p-3 border-0">$1.38</td>
+                    <td class="text-left align-middle p-3 border-0">+2.01</td>
+                    <td class="text-left align-middle p-3 border-0">-0.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAD</td>
+                    <td class="text-left align-middle p-3 border-0">AUSENCO</td>
+                    <td class="text-left align-middle p-3 border-0">$2.38</td>
+                    <td class="text-left align-middle p-3 border-0">-0.01</td>
+                    <td class="text-left align-middle p-3 border-0">-1.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAX</td>
+                    <td class="text-left align-middle p-3 border-0">ADELAIDE</td>
+                    <td class="text-left align-middle p-3 border-0">$3.22</td>
+                    <td class="text-left align-middle p-3 border-0">+0.01</td>
+                    <td class="text-left align-middle p-3 border-0">+1.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">XXD</td>
+                    <td class="text-left align-middle p-3 border-0">ADITYA BIRLA</td>
+                    <td class="text-left align-middle p-3 border-0">$1.02</td>
+                    <td class="text-left align-middle p-3 border-0">-1.01</td>
+                    <td class="text-left align-middle p-3 border-0">+2.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAC</td>
+                    <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
+                    <td class="text-left align-middle p-3 border-0">$1.38</td>
+                    <td class="text-left align-middle p-3 border-0">+2.01</td>
+                    <td class="text-left align-middle p-3 border-0">-0.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAD</td>
+                    <td class="text-left align-middle p-3 border-0">AUSENCO</td>
+                    <td class="text-left align-middle p-3 border-0">$2.38</td>
+                    <td class="text-left align-middle p-3 border-0">-0.01</td>
+                    <td class="text-left align-middle p-3 border-0">-1.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAX</td>
+                    <td class="text-left align-middle p-3 border-0">ADELAIDE</td>
+                    <td class="text-left align-middle p-3 border-0">$3.22</td>
+                    <td class="text-left align-middle p-3 border-0">+0.01</td>
+                    <td class="text-left align-middle p-3 border-0">+1.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">XXD</td>
+                    <td class="text-left align-middle p-3 border-0">ADITYA BIRLA</td>
+                    <td class="text-left align-middle p-3 border-0">$1.02</td>
+                    <td class="text-left align-middle p-3 border-0">-1.01</td>
+                    <td class="text-left align-middle p-3 border-0">+2.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAC</td>
+                    <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
+                    <td class="text-left align-middle p-3 border-0">$1.38</td>
+                    <td class="text-left align-middle p-3 border-0">+2.01</td>
+                    <td class="text-left align-middle p-3 border-0">-0.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAD</td>
+                    <td class="text-left align-middle p-3 border-0">AUSENCO</td>
+                    <td class="text-left align-middle p-3 border-0">$2.38</td>
+                    <td class="text-left align-middle p-3 border-0">-0.01</td>
+                    <td class="text-left align-middle p-3 border-0">-1.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAX</td>
+                    <td class="text-left align-middle p-3 border-0">ADELAIDE</td>
+                    <td class="text-left align-middle p-3 border-0">$3.22</td>
+                    <td class="text-left align-middle p-3 border-0">+0.01</td>
+                    <td class="text-left align-middle p-3 border-0">+1.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">XXD</td>
+                    <td class="text-left align-middle p-3 border-0">ADITYA BIRLA</td>
+                    <td class="text-left align-middle p-3 border-0">$1.02</td>
+                    <td class="text-left align-middle p-3 border-0">-1.01</td>
+                    <td class="text-left align-middle p-3 border-0">+2.36%</td>
+                </tr>
+                <tr>
+                    <td class="text-left align-middle p-3 border-0">AAC</td>
+                    <td class="text-left align-middle p-3 border-0">AUSTRALIAN COMPANY </td>
+                    <td class="text-left align-middle p-3 border-0">$1.38</td>
+                    <td class="text-left align-middle p-3 border-0">+2.01</td>
+                    <td class="text-left align-middle p-3 border-0">-0.36%</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 <div class="container">
-    <div class="table-responsive p-5">
+    <div class="table-responsive py-5">
         <table class="table border-0 table-striped">
             <thead class="thead-dark">
                 <tr>
