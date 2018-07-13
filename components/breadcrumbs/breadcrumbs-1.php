52f2ab0b39f419e291b13a09d8e45afbde006113
Janki Gandhi <jankigandhi@atyantik.com>
2018-07-13 14:45:44 +0530

    designing breadcrumbs

--- components/breadcrumbs/breadcrumbs-1.php
+++ components/breadcrumbs/breadcrumbs-1.php
@@ -23,14 +23,6 @@
   </div>
   <div class="mb-3">
     <h4>Type-3</h4>
-    <ul class="breadcrumb breadcrumb-type2">
-      <li class="breadcrumb-item"><a href="#">Home</a></li>
-      <li class="breadcrumb-item"><a href="#">Library</a></li>
-      <li class="breadcrumb-item active">Data</li>
-    </ul>
-  </div>
-  <div class="mb-3">
-    <h4>Type-4</h4>
     <ul class="breadcrumb breadcrumb-type3">
       <li class="breadcrumb-item"><a href="#">Home</a></li>
       <li class="breadcrumb-item"><a href="#">Library</a></li>
@@ -38,11 +30,67 @@
     </ul>
   </div>
   <div class="mb-3">
-    <h4>Type-5</h4>
+    <h4>Type-4</h4>
     <ul class="breadcrumb breadcrumb-type4">
       <li class="breadcrumb-item"><a href="#">Home</a></li>
       <li class="breadcrumb-item"><a href="#">Library</a></li>
       <li class="breadcrumb-item active">Data</li>
+    </ul>
+  </div>
+  <div class="d-inline-block w-100 mb-3">
+    <h4>Type-5</h4>
+    <ul class="breadcrumb-type5 breadcrumb-primary list-unstyled d-inline-table">
+      <li class="d-inline">
+        <a href="#1" class="active items float-left d-block">
+          <i class="zmdi zmdi-home"></i>
+        </a>
+      </li>
+      <li class="d-inline">
+        <a href="#2" class="items float-left d-block">
+          <i class="zmdi zmdi-shopping-basket"></i>
+          Shop
+        </a>
+      </li>
+      <li class="d-inline">
+        <a href="#3" class="items float-left d-block">
+          <i class="zmdi zmdi-shopping-cart"></i>
+          Cart
+        </a>
+      </li>
+      <li class="d-inline">
+        <a href="#4" class="items float-left d-block">
+          <i class="zmdi zmdi-card"></i>
+          Checkout
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="d-inline-block w-100 mb-3">
+    <h4>Type-6</h4>
+    <ul class="breadcrumb-type5 breadcrumb-danger list-unstyled d-inline-table">
+      <li class="d-inline">
+        <a href="#1" class="items float-left d-block">
+          <i class="zmdi zmdi-home"></i>
+        </a>
+      </li>
+      <li class="d-inline">
+        <a href="#2" class="active items float-left d-block">
+          <i class="zmdi zmdi-shopping-basket"></i>
+          Shop
+        </a>
+      </li>
+      <li class="d-inline">
+        <a href="#3" class="items float-left d-block">
+          <i class="zmdi zmdi-shopping-cart"></i>
+          Cart
+        </a>
+      </li>
+      <li class="d-inline">
+        <a href="#4" class="items float-left d-block">
+          <i class="zmdi zmdi-card"></i>
+          Checkout
+        </a>
+      </li>
     </ul>
   </div>
 </div>
