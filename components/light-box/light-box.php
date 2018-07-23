ace450420481a61f3ea85d6de41e0d75d0ec2750
heli shah <helishah@atyantik.com>
2018-07-23 10:18:13 +0530

    light-box section

--- components/light-box/light-box.php
+++ components/light-box/light-box.php
@@ -5,59 +5,59 @@
 </head>
 <body>
 <div class="container">
-    <h2 style="text-align:center">Lightbox</h2>
+    <h2 class="text-center">Lightbox</h2>
     <div class="row mx-0">
         <div class="col-md-3">
-            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(62).jpg" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(62).jpg" onclick="openModal();currentSlide(1)" class="hover-shadow cursor w-100"/>
         </div>
         <div class="col-md-3">
-            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg" style="width:100%" onclick="openModal();currentSlide(4)" class="hover-shadow cursor">
+            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg" onclick="openModal();currentSlide(2)" class="hover-shadow cursor w-100"/>
         </div>
         <div class="col-md-3">
-            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(63).jpg" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(63).jpg" onclick="openModal();currentSlide(3)" class="hover-shadow cursor w-100"/>
         </div>
         <div class="col-md-3">
-            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(60).jpg" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(60).jpg" onclick="openModal();currentSlide(4)" class="hover-shadow cursor w-100"/>
         </div>
     </div>
     <div id="myModal" class="modal">
-        <span class="close cursor" onclick="closeModal()">&times;</span>
+        <span class="close cursor font-weight-bold" onclick="closeModal()">&times;</span>
         <div class="modal-content">
             <div class="mySlides">
                 <div class="numbertext">1 / 4</div>
-                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(62).jpg" style="width:100%">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(62).jpg" class="w-100"/>
             </div>
             <div class="mySlides">
                 <div class="numbertext">2 / 4</div>
-                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg" style="width:100%">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg" class="w-100"/>
             </div>
             <div class="mySlides">
                 <div class="numbertext">3 / 4</div>
-                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(63).jpg" style="width:100%">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(63).jpg" class="w-100"/>
             </div>
             
             <div class="mySlides">
                 <div class="numbertext">4 / 4</div>
-                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(60).jpg" style="width:100%">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(60).jpg" class="w-100"/>
             </div>
             
             <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
             <a class="next" onclick="plusSlides(1)">&#10095;</a>
-            <div class="caption-container">
+            <div class="caption-container text-center text-white">
                 <p id="caption"></p>
             </div>
-            <div class="row mx-0">
+            <div class="bg-black row mx-0 pb-5">
                 <div class="col-md-3">
-                <img class="demo cursor" src="https://mdbootstrap.com/img/Photos/Slides/img%20(62).jpg" style="width:100%" onclick="currentSlide(1)" alt="Nature and sunrise">
+                <img class="demo cursor w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(62).jpg" onclick="currentSlide(1)" alt="Nature and sunrise"/>
             </div>
             <div class="col-md-3">
-                <img class="demo cursor" src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg" style="width:100%" onclick="currentSlide(2)" alt="Snow">
+                <img class="demo cursor w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg" onclick="currentSlide(2)" alt="Snow"/>
             </div>
             <div class="col-md-3">
-                <img class="demo cursor" src="https://mdbootstrap.com/img/Photos/Slides/img%20(63).jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+                <img class="demo cursor w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(63).jpg" onclick="currentSlide(3)" alt="Mountains and fjords"/>
             </div>
             <div class="col-md-3">
-                <img class="demo cursor" src="https://mdbootstrap.com/img/Photos/Slides/img%20(60).jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
+                <img class="demo cursor w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(60).jpg" onclick="currentSlide(4)" alt="Northern Lights"/>
             </div>
             </div>
         </div>
