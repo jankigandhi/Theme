68b4e06cc0fef8be996f7f53e92e3c7e24b7cb60
Janki Gandhi <jankigandhi@atyantik.com>
2018-07-24 11:53:16 +0530

    design changes

--- components/call-to-actions/call-actions.php
+++ components/call-to-actions/call-actions.php
@@ -5,144 +5,146 @@
 </head>
 <body>
     <div class="container">
-        <div class="max-width mx-auto">
+        <div class="max-width-1000 mx-auto">
             <h1 class="h2 text-center mt-3">Call to Actions</h1>
-            <h4 class="my-4">Default</h4>
-            <div class="row call-to-action my-5 mx-0 align-items-center justify-content-md-between justify-content-center">
-                <div class="text-center">
-                    <span class="ti-icon ">
-                        <i class="ti-shopping-cart text-primary"></i>
-                    </span>
-                </div>
-                <div class="">
-                    <div class="call-to-action-content text-center">
-                        <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
-                        <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center justify-content-md-end">
-                    <button class="btn-thick rounded-0 btn btn-primary">Buy Now!</button>
-                </div>
-            </div>
-            <h4 class="my-4">With Borders</h4>
-            <div class="row call-to-action with-border rounded my-5 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
-                <div>
-                    <span class="ti-icon text-success">
-                        <i class="ti-world "></i>
-                    </span>
-                </div>
-                <div>
-                    <div class="call-to-action-content text-center">
-                        <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
-                        <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center justify-content-md-end">
-                    <button class="btn-thick rounded-0 btn btn-success">Buy Now!</button>
-                </div>
-            </div>
-            <h4 class="my-4">Featured</h4>
-            <div class="border-top border-primary rounded border-thick my-3">
-                <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
+            <div>
+                <div class="row call-to-action my-3 mx-0 align-items-center justify-content-md-between justify-content-center">
                     <div>
-                        <span class="ti-icon text-primary">
-                            <i class="ti-comment"></i>
+                        <span class="ti-icon">
+                            <i class="ti-shopping-cart text-primary"></i>
                         </span>
                     </div>
                     <div>
-                        <div class="call-to-action-content text-center">
+                        <div class="text-center">
                             <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
                             <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-center justify-content-md-end">
-                        <button class="btn-thick rounded-0 btn btn-outline-primary">Buy Now!</button>
+                    <div>
+                        <button class="btn-thick rounded-0 btn btn-primary">Buy Now!</button>
                     </div>
                 </div>
             </div>
-            <div class="border-top border-success rounded border-thick my-3">
-                <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
+            <div>
+                <h4>With Borders</h4>
+                <div class="row call-to-action with-border rounded my-3 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
                     <div>
                         <span class="ti-icon text-success">
-                            <i class="ti-flag-alt"></i>
+                            <i class="ti-world "></i>
                         </span>
                     </div>
                     <div>
-                        <div class="call-to-action-content text-center">
+                        <div class="text-center">
                             <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
                             <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-center justify-content-md-end">
-                        <button class="btn-thick rounded-0 btn btn-outline-success">Buy Now!</button>
+                    <div>
+                        <button class="btn-thick rounded-0 btn btn-success">Buy Now!</button>
                     </div>
                 </div>
             </div>
-            <div class="border-top border-info rounded border-thick my-3">
-                <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
-                    <div>
-                        <span class="ti-icon text-info">
-                            <i class="ti-headphone"></i>
-                        </span>
-                    </div>
-                    <div>
-                        <div class="call-to-action-content text-center">
-                            <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
-                            <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+            <div>
+                <h4>Featured</h4>
+                <div class="border-top border-primary rounded border-thick my-3">
+                    <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
+                        <div>
+                            <span class="ti-icon text-primary">
+                                <i class="ti-comment"></i>
+                            </span>
                         </div>
-                    </div>
-                    <div class="d-flex justify-content-center justify-content-md-end">
-                        <button class="btn-thick rounded-0 btn btn-outline-info">Buy Now!</button>
+                        <div>
+                            <div class="text-center">
+                                <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
+                                <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="btn-thick rounded-0 btn btn-outline-primary">Buy Now!</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="border-top border-danger rounded border-thick my-3">
-                <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
-                    <div>
-                        <span class="ti-icon text-danger">
-                            <i class="ti-car"></i>
-                        </span>
-                    </div>
-                    <div>
-                        <div class="call-to-action-content text-center">
-                            <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
-                            <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+                <div class="border-top border-success rounded border-thick my-3">
+                    <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
+                        <div>
+                            <span class="ti-icon text-success">
+                                <i class="ti-flag-alt"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <div class="text-center">
+                                <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
+                                <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="btn-thick rounded-0 btn btn-outline-success">Buy Now!</button>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-center justify-content-md-end">
-                        <button class="btn-thick rounded-0 btn btn-outline-danger">Buy Now!</button>
+                </div>
+                <div class="border-top border-info rounded border-thick my-3">
+                    <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
+                        <div>
+                            <span class="ti-icon text-info">
+                                <i class="ti-headphone"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <div class="text-center">
+                                <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
+                                <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="btn-thick rounded-0 btn btn-outline-info">Buy Now!</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="border-top border-danger rounded border-thick my-3">
+                    <div class="row call-to-action with-border border-top-0 mx-0 align-items-center p-3 justify-content-md-between justify-content-center">
+                        <div>
+                            <span class="ti-icon text-danger">
+                                <i class="ti-car"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <div class="text-center">
+                                <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
+                                <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="btn-thick rounded-0 btn btn-outline-danger">Buy Now!</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <h4 class="my-4 px-5">Parallax</h4>
-    <div class="banner-images py-5" style="background-image: url('images/parallax-2.jpg');">
-        <div class="container">
-            <div class="row call-to-action my-5 mx-0 align-items-center justify-content-md-between justify-content-center">
-                <div>
-                    <span class="ti-icon light-gray">
-                        <i class="ti-game"></i>
-                    </span>
-                </div>
-                <div>
-                    <div class="call-to-action-content text-center">
-                        <h3 class="call-to-action-text text-white">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
-                        <p class="call-to-action-desc light-gray text-white">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
+    <div>
+        <h4 class="px-5">Parallax</h4>
+        <div class="banner-images py-5" style="background-image: url('images/parallax-2.jpg');">
+            <div class="container">
+                <div class="call-to-action py-5">
+                    <div class="text-center">
+                        <div>
+                            <h3 class="text-white">
+                            Authentic, handcrafted design that gives your
+                            <span class="d-block">site subtle, confident appeal.</span>
+                            </h3>
+                        </div>
+                        <button class="btn-thick btn-min-w rounded-0 btn btn-primary mt-3">Buy Now!</button>
                     </div>
-                </div>
-                <div class="d-flex justify-content-center justify-content-md-end">
-                    <button class="btn-thick rounded-0 btn btn-primary">Buy Now!</button>
                 </div>
             </div>
         </div>
     </div>
     <div class="container">
-        <h4 class="my-4">Small</h4>
-        <div class="row my-5 mx-0">
+        <h4 class="m-3">Small</h4>
+        <div class="row mb-4 mx-0">
             <div class="col-lg-4 my-3 my-lg-0">
                 <div class="call-to-action with-border rounded p-5">
-                    <div class="call-to-action-content text-center">
+                    <div class="text-center">
                         <h3 class="call-to-action-text">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
                         <p class="call-to-action-desc light-gray">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
                     </div>
@@ -153,7 +155,7 @@
             </div>
             <div class="col-lg-4 my-3 my-lg-0">
                 <div class="call-to-action bg-primary rounded p-5">
-                    <div class="call-to-action-content text-center">
+                    <div class="text-center">
                         <h3 class="call-to-action-text text-white">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
                         <p class="call-to-action-desc light-gray text-white">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
                     </div>
@@ -164,7 +166,7 @@
             </div>
             <div class="col-lg-4 my-3 my-lg-0">
                 <div class="call-to-action bg-dark rounded p-5">
-                    <div class="call-to-action-content text-center">
+                    <div class="text-center">
                         <h3 class="call-to-action-text text-white">Porto is <strong>everything</strong> you need to create a <strong>website!</strong></h3>
                         <p class="call-to-action-desc light-gray text-white">The <strong>#1 Selling</strong> HTML Site Template on ThemeForest</p>
                     </div>
