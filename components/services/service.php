2f98e20bc678e24da336b61e2537b9cb7212c02f
heli shah <helishah@atyantik.com>
2018-07-25 13:03:33 +0530

    designing footer

---
+++ components/services/service.php
@@ -0,0 +1,29 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../typography/css/typography.css">
+<link rel="stylesheet" href="../Buttons/css/button.css">
+<link rel="stylesheet" type="text/css" href="css/services.css">
+</head>
+<body>
+<?php $HederClass="";?>
+<?php include_once '../nav-bar/nav-bar.php';?>
+    <div class="">
+        <div class="banner img" style="background-image: url('images/banner-3.jpg');">
+            <div class="banner-content">
+                <div class="d-flex justify-content-between text-white">
+                    <div>
+                        <p>Services</p>
+                    </div>
+                    <div>
+                        <ul class="list-unstyled mb-0 w-100 d-inline-block">
+                            <li class="d-inline-block">Home /</li>
+                            <li class="d-inline-block">Pages /</li>
+                            <li class="d-inline-block">Services </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include_once '../../inc/footer.php';?>
+</body>
+</html>
