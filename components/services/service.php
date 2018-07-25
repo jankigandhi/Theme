31192970ce0f97d3116974a0eeea81a43912d7b7
heli shah <helishah@atyantik.com>
2018-07-25 15:46:52 +0530

    designing service page

--- components/services/service.php
+++ components/services/service.php
@@ -1,29 +1,100 @@
 <?php include_once '../../inc/head.php';?>
 <link rel="stylesheet" href="../typography/css/typography.css">
 <link rel="stylesheet" href="../Buttons/css/button.css">
+<link rel="stylesheet" href="../breadcrumbs/css/breadcrumbs.css">
 <link rel="stylesheet" type="text/css" href="css/services.css">
 </head>
 <body>
 <?php $HederClass="";?>
 <?php include_once '../nav-bar/nav-bar.php';?>
-    <div class="">
-        <div class="banner img" style="background-image: url('images/banner-3.jpg');">
-            <div class="banner-content">
-                <div class="d-flex justify-content-between text-white">
-                    <div>
+<div>
+    <div class="banner img" style="background-image: url('images/banner-3.jpg');">
+        <div class="banner-content">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="text-white">
                         <p>Services</p>
                     </div>
-                    <div>
-                        <ul class="list-unstyled mb-0 w-100 d-inline-block">
-                            <li class="d-inline-block">Home /</li>
-                            <li class="d-inline-block">Pages /</li>
-                            <li class="d-inline-block">Services </li>
+                    <div class="text-right">
+                        <ul class="items breadcrumb">
+                            <li class="slash-icon breadcrumb-item">
+                                <a href="#" class="text-white">
+                                    <i class="zmdi zmdi-home"></i>
+                                    Home
+                                </a>
+                            </li>
+                            <li class="slash-icon breadcrumb-item">
+                                <a href="#" class="text-white">
+                                    Pages
+                                </a>
+                            </li>
+                            <li class="slash-icon breadcrumb-item active text-white">Services</li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<div>
+    <div class="text-center my-5">
+        <h2>Create something beautiful.</h2>
+        <p>Varient Page Builder, Over 100 Page Templates - The choice is clear. </p>
+        <div>
+            <button type="button" class="btn-effect-3 effect-1 btn-min-w btn-thick rounded-0 btn btn-success">
+            <span class="btn-text">Purchase Theme</span>
+            <span class="btn-icon">
+                <i class="zmdi zmdi-shopping-cart"></i>
+            </span>
+            </button>
+        </div>
+    </div>
+</div>
+<div>
+    <div class="banner service-img" style="background-image: url('images/product-banner.png');">
+        <div class="banner-content">
+            <div class="container">
+                <h3 class="text-center text-white">There's so much to love</h3>
+                <p class="text-center text-white">A remarkably versatile template packed with features.</p>
+                <div class="row mx-0 mb-5">
+                    <div class="col-sm-4 pb-3">
+                        <div class="service-box p-3">
+                            <div class="text-center">
+                                <span class="icon"><i class="ti-gallery text-white"></i></span>
+                                <h5 class="text-uppercase text-white">Unique concepts</h5>
+                            </div>
+                            <p class="text-white">
+                                10 Fresh and unique concepts included out of the box. From portfolio to property showcase, Foundry's adaptable look is perfect for your next project.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 pb-3">
+                        <div class="service-box p-3">
+                            <div class="text-center">
+                                <span class="icon"><i class="ti-server text-white"></i></span>
+                                <h5 class="text-uppercase text-white">Developer concepts</h5>
+                            </div>
+                            <p class="text-white">
+                                10 Fresh and unique concepts included out of the box. From portfolio to property showcase, Foundry's adaptable look is perfect for your next project.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 pb-3">
+                        <div class="service-box p-3">
+                            <div class="text-center">
+                                <span class="icon"><i class="ti-package text-white"></i></span>
+                                <h5 class="text-uppercase text-white">Unique concepts</h5>
+                            </div>
+                            <p class="text-white">
+                                10 Fresh and unique concepts included out of the box. From portfolio to property showcase, Foundry's adaptable look is perfect for your next project.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include_once '../../inc/footer.php';?>
 </body>
 </html>
