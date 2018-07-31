5cbe065cda3cff32357d0e75923b8d84a8bc09f2
Janki Gandhi <jankigandhi@atyantik.com>
2018-07-31 17:42:53 +0530

    css changes

--- components/nav-bar/nav-bar-1.php
+++ components/nav-bar/nav-bar-1.php
@@ -1,8 +1,8 @@
 <div class="site-header">
   <div class="top-bar bg-dark">
-    <div class="container">
-      <div class="d-inline-block w-100">
-        <div class="float-left">
+    <div class="container px-0 px-lg-3">
+      <div class="d-inline-block w-100 text-center text-lg-left">
+        <div class="top-bar-bottom-line float-lg-left">
           <button class="top-bar-link hover-links btn btn-link rounded-0">
           <i class="pr-1 fa fa-user"></i>
           Agent Login
@@ -19,11 +19,11 @@
           Call US Now:815-123-4567
           </button>
         </div>
-        <div class="float-right">
-          <a href="#" class="top-bar-link hover-links btn btn-link"><i class="fab fa-facebook-f"></i></a>
-          <a href="#" class="top-bar-link hover-links btn btn-link"><i class="fab fa-twitter"></i></a>
-          <a href="#" class="top-bar-link hover-links btn btn-link"><i class="fab fa-tumblr"></i></a>
-          <a href="#" class="top-bar-link hover-links btn btn-link"><i class="fab fa-pinterest"></i></a>
+        <div class="float-lg-right">
+          <a href="#" class="top-bar-link hover-links btn btn-link rounded-0"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" class="top-bar-link hover-links btn btn-link rounded-0"><i class="fab fa-twitter"></i></a>
+          <a href="#" class="top-bar-link hover-links btn btn-link rounded-0"><i class="fab fa-tumblr"></i></a>
+          <a href="#" class="top-bar-link hover-links btn btn-link rounded-0"><i class="fab fa-pinterest"></i></a>
         </div>
       </div>
     </div>
@@ -103,48 +103,114 @@
               <li class="menu-col">
                 <ul class="list-unstyled">
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Typography</a>
+                    <span class="hover-links title text-uppercase font-weight-bold">Elements</span>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Buttons</a>
+                    <a href="#" class="hover-links text-uppercase">Typography</a>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Alert</a>
+                    <a href="#" class="hover-links text-uppercase">Buttons</a>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Grid</a>
+                    <a href="#" class="hover-links text-uppercase">Alert</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Grid</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Card</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <span class="hover-links title text-uppercase font-weight-bold">Image Gallery</span>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Gallery1</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Gallery2</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Gallery3</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Gallery4</a>
                   </li>
                 </ul>
               </li>
               <li class="menu-col">
                 <ul class="list-unstyled">
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Typography</a>
+                    <span class="hover-links title text-uppercase font-weight-bold">Elements</span>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Buttons</a>
+                    <a href="#" class="hover-links text-uppercase">Accordions</a>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Alert</a>
+                    <a href="#" class="hover-links text-uppercase">Pagination</a>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Grid</a>
+                    <a href="#" class="hover-links text-uppercase">Progressbar</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Process steps</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Breadcrumbs</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Testimonial</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Modals</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Call To Actions</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Form Input</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Table</a>
                   </li>
                 </ul>
               </li>
               <li class="menu-col">
                 <ul class="list-unstyled">
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Typography</a>
+                    <span class="hover-links title text-uppercase font-weight-bold">Elements</span>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Buttons</a>
+                    <span class="hover-links title text-uppercase font-weight-bold">Navbar</span>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Alert</a>
+                    <a href="#" class="hover-links text-uppercase">Default navbar</a>
                   </li>
                   <li class="sub-menu-link">
-                    <a href="#" class="hover-links text-uppercase font-weight-bold">Grid</a>
+                    <a href="#" class="hover-links text-uppercase">Dark navbar</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Transparent navbar</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <span class="hover-links title text-uppercase font-weight-bold">Slider</span>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Banner Slider</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Banner Slider1</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <span class="hover-links title text-uppercase font-weight-bold">Google Maps</span>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Maps</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Full height Map</a>
+                  </li>
+                  <li class="sub-menu-link">
+                    <a href="#" class="hover-links text-uppercase">Map with form</a>
                   </li>
                 </ul>
               </li>
@@ -152,10 +218,18 @@
           </li>
           <li class="header-links link-arrow d-inline-block">
             <a href="#" class="links hover-links text-uppercase font-weight-bold">Contact Us</a>
+            <ul class="sub-menu list-unstyled">
+              <li class="sub-menu-link">
+                <a href="#" class="hover-links text-uppercase">Contact1</a>
+              </li>
+              <li class="sub-menu-link">
+                <a href="#" class="hover-links text-uppercase">Contact2</a>
+              </li>
+            </ul>
           </li>
         </ul>
         <div class="d-inline-block px-3">
-          <button class="btn-search btn btn-outline-dark rounded-circle d-flex btn-thick p-0 justify-content-center align-items-center">
+          <button class="btn-search btn btn-outline-secondary rounded-circle d-flex btn-thick p-0 justify-content-center align-items-center">
           <i class="fas fa-search"></i>
           </button>
         </div>
