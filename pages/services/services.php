54205a7137149f6bfde8b71b0cc2f6d5c623ecb1
Harsh Sharma <jankigandhi@atyantik.com>
2018-08-09 15:26:46 +0530

    design services page

---
+++ pages/services/services.php
@@ -0,0 +1,45 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="css/services.css">
+</head>
+<body>
+<?php $HederClass="header-transparent";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+	<div class="banner">
+		<div class="banner-img" style="background-image: url('../../images/banner-2.png');">
+			<div class="container">
+				<div class="services-banner">
+					<h2 class="text-white mb-0 text-center my-5 font-weight-bold text-uppercase">Services</h2>
+					<P class="services-desc mx-auto mb-0 text-white text-center">
+						Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+					</P>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="services-section">
+		<div class="container">
+			<div class="icon-with-box py-5">
+				<div class="row mx-0">
+					<div class="col-md-6">
+						<div class="row mx-0">
+							<div class="col-md-2 text-center">
+								<span class="services-icon text-primary">
+									<i class="fas fa-sliders-h"></i>
+								</span>
+							</div>
+							<div class="col-md-10">
+								<h3 class="services-title">Brand Identity</h3>
+								<p class="box-desc">Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+</body>
+</html>
