3a1a734df04b887b34134a1eacb011057e6fe938
Harsh Sharma <jankigandhi@atyantik.com>
2018-08-09 19:03:20 +0530

    services page design

--- pages/services/services.php
+++ pages/services/services.php
@@ -1,5 +1,6 @@
 <?php include_once '../../inc/head.php';?>
 <link rel="stylesheet" href="css/services.css">
+<link rel="stylesheet" href="../../css/libs/swiper.min.css">
 </head>
 <body>
 <?php $HederClass="header-transparent";?>
@@ -10,26 +11,72 @@
 			<div class="container">
 				<div class="services-banner">
 					<h2 class="text-white mb-0 text-center my-5 font-weight-bold text-uppercase">Services</h2>
-					<P class="services-desc mx-auto mb-0 text-white text-center">
+					<P class="mw-600 mx-auto mb-0 text-white text-center">
 						Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
 					</P>
+					<div class="my-4">
+						<button type="button" class="btn-thick btn-min-w btn btn-outline-primary mx-1">Primary</button>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 	<div class="services-section">
 		<div class="container">
-			<div class="icon-with-box py-5">
+			<div class="icon-with-box">
+				<div class="my-5">
+					<p class="text-primary text-center text-uppercase font-weight-bold mb-0">What We Do</p>
+					<h3 class="service-title mw-600 mx-auto text-center font-weight-medium py-3 mb-0">We’ve got everything you need to launch and grow your business</h3>
+				</div>
 				<div class="row mx-0">
-					<div class="col-md-6">
+					<div class="col-md-6 py-4">
 						<div class="row mx-0">
 							<div class="col-md-2 text-center">
 								<span class="services-icon text-primary">
 									<i class="fas fa-sliders-h"></i>
 								</span>
 							</div>
-							<div class="col-md-10">
-								<h3 class="services-title">Brand Identity</h3>
+							<div class="col-md-10 text-center text-md-left">
+								<h3 class="font-weight-medium mb-3">Brand Identity</h3>
+								<p class="box-desc">Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 py-4">
+						<div class="row mx-0">
+							<div class="col-md-2 text-center">
+								<span class="services-icon text-primary">
+									<i class="fas fa-wrench"></i>
+								</span>
+							</div>
+							<div class="col-md-10 text-center text-md-left">
+								<h3 class="font-weight-medium mb-3">Illustration</h3>
+								<p class="box-desc">Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 py-4">
+						<div class="row mx-0">
+							<div class="col-md-2 text-center">
+								<span class="services-icon text-primary">
+									<i class="fas fa-cog"></i>
+								</span>
+							</div>
+							<div class="col-md-10 text-center text-md-left">
+								<h3 class="font-weight-medium mb-3">Marketing</h3>
+								<p class="box-desc">Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 py-4">
+						<div class="row mx-0">
+							<div class="col-md-2 text-center">
+								<span class="services-icon text-primary">
+									<i class="fas fa-briefcase"></i>
+								</span>
+							</div>
+							<div class="col-md-10 text-center text-md-left">
+								<h3 class="font-weight-medium mb-3">Web Development</h3>
 								<p class="box-desc">Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
 							</div>
 						</div>
@@ -38,8 +85,169 @@
 			</div>
 		</div>
 	</div>
+	<div class="slider-section py-5">
+		<div class="container">
+			<p class="text-center font-weight-bold text-primary text-uppercase">Our Clients</p>
+			<h3 class="mw-600 mx-auto text-center font-weight-bold">Glint has been honored to partner up with these clients</h3>
+			<div class="swiper-container py-4">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-1.png" alt="Slide-1" />
+						</a>
+					</div>
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-2.png" alt="slide-2" />
+						</a>
+					</div>
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-3.png" alt="slide-3" />
+						</a>
+					</div>
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-4.png" alt="slide-4" />
+						</a>
+					</div>
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-5.png" alt="slide-5" />
+						</a>
+					</div>
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-6.png" alt="slide-6" />
+						</a>
+					</div>
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-7.png" alt="slide-7" />
+						</a>
+					</div>
+					<div class="swiper-slide text-center">
+						<a href="#">
+							<img class="slide-icon" src="images/slide-8.png" alt="slide-8" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="features-section my-5">
+		<div class="container">
+			<div class="text-center py-4">
+				<h3 class="font-weight-bold">Some Features that Made us Unique</h3>
+				<p class="font-weight-medium">Who are in extremely love with eco friendly system.</p>
+			</div>
+			<div class="row mx-0">
+				<div class="col-lg-4 col-md-6">
+					<div class="features-box p-4">
+						<div class="d-inline-block py-3">
+							<span class="features-icon">
+								<i class="fas fa-user"></i>
+							</span>
+							<span class="pl-3 font-weight-medium">Expert Technicians</span>
+						</div>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="features-box p-4">
+						<div class="d-inline-block py-3">
+							<span class="features-icon">
+								<i class="fas fa-address-card"></i>
+							</span>
+							<span class="pl-3 font-weight-medium">Professional Service</span>
+						</div>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="features-box p-4">
+						<div class="d-inline-block py-3">
+							<span class="features-icon">
+								<i class="fas fa-mobile"></i>
+							</span>
+							<span class="pl-3 font-weight-medium">Great Support</span>
+						</div>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="features-box p-4">
+						<div class="d-inline-block py-3">
+							<span class="features-icon">
+								<i class="fas fa-rocket"></i>
+							</span>
+							<span class="pl-3 font-weight-medium">Technical Skills</span>
+						</div>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="features-box p-4">
+						<div class="d-inline-block py-3">
+							<span class="features-icon">
+								<i class="fas fa-gem"></i>
+							</span>
+							<span class="pl-3 font-weight-medium">Highly Recomended</span>
+						</div>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="features-box p-4">
+						<div class="d-inline-block py-3">
+							<span class="features-icon">
+								<i class="fas fa-comment"></i>
+							</span>
+							<span class="pl-3 font-weight-medium">Positive Reviews</span>
+						</div>
+						<p>
+							Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
 <?php include_once '../../components/footer/footer.php';?>
 <?php include_once '../../inc/foot.php';?>
+<script src="../../js/swiper.min.js"></script>
+<script>
+	var swiper = new Swiper('.swiper-container', {
+	slidesPerView: 6,
+	spaceBetween: 20,
+	speed: 750,
+	grabCursor: true,
+	autoplay: {
+	delay: 3000,
+	},
+	loop:true,
+	breakpoints: {
+	1000: {
+	slidesPerView: 4,
+	spaceBetween: 20
+	},
+	768: {
+	slidesPerView: 2,
+	spaceBetween: 20
+	},
+	}
+	});
+</script>
 </body>
 </html>
