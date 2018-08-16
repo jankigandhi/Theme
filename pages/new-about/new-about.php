2dda62b1afed8bdbfad820b5a4b810cdde4b53b2
Harsh Sharma <jankigandhi@atyantik.com>
2018-08-16 14:58:45 +0530

    design changes

---
+++ pages/new-about/new-about.php
@@ -0,0 +1,210 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../../components/typography/css/typography.css">
+<link rel="stylesheet" href="../../components/Buttons/css/button.css">
+<link rel="stylesheet" href="../../css/libs/swiper.min.css">
+<link rel="stylesheet" type="text/css" href="../../components/form-input/css/form-input.css">
+<link rel="stylesheet" type="text/css" href="../../components/map/css/map.css">
+<link rel="stylesheet" type="text/css" href="../../components/img-gallery/css/gallery.css">
+<link rel="stylesheet" type="text/css" href="../../components/nav-bar/css/nav-bar.css">
+<link rel="stylesheet" type="text/css" href="../../components/card/css/card.css">
+<link rel="stylesheet" type="text/css" href="../../components/modal/css/modal.css">
+<link rel="stylesheet" type="text/css" href="../../components/footer/css/footer.css">
+<link rel="stylesheet" href="../../pages/new-about/css/new-about.css">
+</head>
+<body>
+<?php $HederClass="";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+	<div class="banner-section">
+		<div class="about-banner-section" style="background-image: url('images/about-banner.jpg');">
+			<div class="banner-text">
+				<h2 class="fadeInDown text-white text-uppercase font-weight-bold text-center">Lorem Ipsum Dolor</h2>
+				<p class="sub-title text-white text-center my-4 fadeInDown">Sed lorem consequat feugiat</p>
+				<div class="text-center">
+					<button type="button" class="btn-thick btn-min-w rounded-0 btn btn-outline-primary text-center">Magna Sed Etiam</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="box-grid">
+		<div class="container">
+			<div class="py-5">
+				<h3 class="font-weight-bold text-uppercase text-center">Magna feugiat</h3>
+				<p class="text-center px-5 font-weight-bold">Aliquet ante lobortis non semper gravida. Accumsan faucibus praesent ante aliquet amet posuere ac cubilia. Eu faucibus mi neque adipiscing mi lorem. Semper blandit. Amet adipiscing interdum.</p>
+				<div>
+					<div class="row my-5 mx-0 justify-content-md-center">
+						<div class="col-lg-4 col-md-6 py-3">
+							<div class="box-spacial bg-white">
+								<div class="text-center p-4">
+									<span class="circle-round rounded-circle d-inline-block">
+										<i class="zmdi zmdi-email icon-user font-weight-normal text-white"></i>
+									</span>
+									<h3 class="box-title text-uppercase font-weight-medium text-gray my-4">Ipsum Feugiat</h3>
+									<p class="box-desc text-gray font-weight-medium">Morbi in sem quis dui placerat ornare lorem pellentesque odio nisi sed euismod. In tempus posuere augue eu magna adipiscing.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 py-3">
+							<div class="box-spacial bg-white">
+								<div class="text-center p-4">
+									<span class="circle-round rounded-circle d-inline-block">
+										<i class="zmdi zmdi-airplane icon-user font-weight-normal text-white"></i>
+									</span>
+									<h3 class="box-title text-uppercase font-weight-medium text-gray my-4">Nunc Dignis</h3>
+									<p class="box-desc text-gray font-weight-medium">Morbi in sem quis dui placerat ornare lorem pellentesque odio nisi sed euismod. In tempus posuere augue eu magna adipiscing.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 py-3">
+							<div class="box-spacial bg-white">
+								<div class="text-center p-4">
+									<span class="circle-round rounded-circle d-inline-block">
+										<i class="zmdi zmdi-flash icon-user font-weight-normal text-white"></i>
+									</span>
+									<h3 class="box-title text-uppercase font-weight-medium text-gray my-4">Integer Vita</h3>
+									<p class="box-desc text-gray font-weight-medium">Morbi in sem quis dui placerat ornare lorem pellentesque odio nisi sed euismod. In tempus posuere augue eu magna adipiscing.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="about-hover-section">
+		<div class="container">
+			<div>
+				<h3 class="text-center py-4">Sed Faucibus</h3>
+				<div class="row mx-0 justify-content-md-center">
+					<div class="col-lg-4 col-md-6 py-3">
+						<a href="images/gallery-1.png" class="img-block d-block glightbox">
+							<div class="gallery-img hover-effect" style="background-image: url('images/gallery-img-1.jpg');">
+								<button type="button" class="btn-more btn-thick btn-min-w rounded-0 btn btn-outline-primary">Learn More</button>
+							</div>
+						</a>
+						<div class="text-center">
+							<h4 class="my-3">Etiam Interdum</h4>
+							<p class="px-2">Praesent dapibus, neque id cursus tortor magna etiam lorem neque egestas sed magna tempus.</p>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 py-3">
+						<a href="images/gallery-1.png" class="img-block d-block glightbox">
+							<div class="gallery-img hover-effect" style="background-image: url('images/gallery-img.jpg');">
+								<button type="button" class="btn-more btn-thick btn-min-w rounded-0 btn btn-outline-primary">Learn More</button>
+							</div>
+						</a>
+						<div class="text-center">
+							<h4 class="my-3">Varius Faucibus</h4>
+							<p class="px-2">Praesent dapibus, neque id cursus tortor magna etiam lorem neque egestas sed magna tempus.</p>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 py-3">
+						<a href="images/gallery-1.png" class="img-block d-block glightbox">
+							<div class="gallery-img hover-effect" style="background-image: url('images/gallery-img-2.jpg');">
+								<button type="button" class="btn-more btn-thick btn-min-w rounded-0 btn btn-outline-primary">Learn More</button>
+							</div>
+						</a>
+						<div class="text-center">
+							<h4 class="my-3">Ante Praesent</h4>
+							<p class="px-2">Praesent dapibus, neque id cursus tortor magna etiam lorem neque egestas sed magna tempus.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contact-section">
+		<h4 class="text-center my-4 mb-0">Map With contact-form</h4>
+		<div class="map-with-form map-vh">
+			<div id="map" class="h-100">
+			</div>
+			<button class="btn-connect btn btn-primary d-sm-none">
+			<i class="fas fa-comments"></i>
+			</button>
+			<div class="contact-form p-4">
+				<button class="btn-close btn btn-link d-sm-none float-right font-weight-bold">
+				<span class="ti-close"></span>
+				</button>
+				<h4 class="title text-uppercase font-weight-bold mb-4">Connect</h4>
+				<ul class="social-section list-unstyled mb-0 pb-3">
+					<li class="d-inline-block align-top mr-3">
+						<a href="#" class="social-icon"><i class="zmdi zmdi-facebook"></i></a>
+					</li>
+					<li class="d-inline-block align-top mr-3">
+						<a href="#" class="social-icon"><i class="zmdi zmdi-twitter"></i></a>
+					</li>
+					<li class="d-inline-block align-top mr-3">
+						<a href="#" class="social-icon"><i class="zmdi zmdi-google-plus"></i></a>
+					</li>
+					<li class="d-inline-block align-top mr-3">
+						<a href="#" class="social-icon"><i class="zmdi zmdi-rss"></i></a>
+					</li>
+				</ul>
+				<div class="form-section py-4">
+					<h5 class="title text-uppercase font-weight-bold mb-4">Send a message</h5>
+					<form>
+						<div class="form-group">
+							<input type="email" class="square-box form-control" placeholder="Email" />
+						</div>
+						<div class="form-group">
+							<input type="text" class="square-box form-control" placeholder="Subject" />
+						</div>
+						<div class="input-area input-effect mb-3">
+							<textarea class="square-box form-control" placeholder="Write Here..." rows="8"></textarea>
+						</div>
+						<div>
+							<button type="button" class="btn-thick rounded-0 btn btn-primary mb-0 btn-block">
+							Send
+							</button>
+						</div>
+					</form>
+				</div>
+				<div class="py-2">
+					<p>
+						<i class="email-icon zmdi zmdi-email pr-2"></i>
+						<a href="mailto:contact@atyantik.com" class="email">contact@atyantik.com</a>
+					</p>
+					<p>
+						<i class="ph-icon zmdi zmdi-phone pr-2"></i>
+						<a href="tel:+91-7600540105" class="phone">+91-7600540105</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCX-gnjI10taggMKTIc8jNd6VAobfU3leY" type="text/javascript"></script>
+<script src="../../js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript">
+	var map;
+	var locations = [
+	['Bondi Beach', -33.890542, 151.274856, 4],
+	// ['Coogee Beach', -33.923036, 151.259052, 5],
+	// ['Cronulla Beach', -34.028249, 151.157507, 3],
+	// ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+	// ['Maroubra Beach', -33.950198, 151.259302, 1]
+	];
+	var map = new google.maps.Map(document.getElementById('map'), {
+	zoom: 10,
+	center: new google.maps.LatLng(-33.92, 151.25),
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+	var infowindow = new google.maps.InfoWindow();
+	var marker, i;
+	for (i = 0; i < locations.length; i++) {
+	marker = new google.maps.Marker({
+	position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+	map: map
+	});
+	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	return function() {
+	infowindow.setContent(locations[i][0]);
+	infowindow.open(map, marker);
+	}
+	})(marker, i));
+	}
+</script>
+</body>
+</html>
