e04ac749bf7c0a83542db1ee518aeaab84a61921
Harsh Sharma <jankigandhi@atyantik.com>
2018-08-20 18:37:13 +0530

    services page design changes

---
+++ pages/new-services/new-services.php
@@ -0,0 +1,14 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../../components/card/css/card.css">
+<link rel="stylesheet" href="css/new-services.css">
+</head>
+<body>
+<?php $HederClass="header-transparent";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+    
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+</body>
+</html>
