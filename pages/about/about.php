71cd8fd2ba649fb4d6d72c8da0383fe39cc51ad6
Janki Gandhi <jankigandhi@atyantik.com>
2018-08-20 18:30:21 +0530

    designing about page

---
+++ pages/about/about.php
@@ -0,0 +1,83 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" type="text/css" href="../../components/breadcrumbs/css/breadcrumbs.css">
+<link rel="stylesheet" href="css/about.css">
+</head>
+<body>
+<?php $HederClass="fixed-header";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+  <div class="banner">
+    <div class="banner-img" style="background-image: url('https://images.alphacoders.com/498/498263.jpg');"></div>
+    <div class="banner-content">
+      <div class="center-content">
+        <div class="container">
+          <h3 class="text-white text-uppercase">About Us</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="shadow-sm">
+    <div class="container">
+      <ul class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">
+            <i class="zmdi zmdi-home"></i>
+            Home
+          </a>
+        </li>
+        <li class="breadcrumb-item active">About Us</li>
+      </ul>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row py-4">
+      <div class="col-md-7 py-2">
+        <h3 class="text-uppercase">Lorem ipsum is simply</h3>
+        <p class="light-gray">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </div>
+      <div class="col-md-5 py-2">
+        <div class="about-bg-img" style="background-image: url('https://images.alphacoders.com/498/498263.jpg');"></div>
+      </div>
+    </div>
+  </div>
+  <div class="work-section text-center py-5">
+    <div class="container">
+      <h3 class="text-uppercase">How it Works</h3>
+      <p>Absolutely Stunning Design and Functionality</p>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="icon">
+            <i class="fas fa-hotel"></i>
+          </div>
+          <h5>Planning</h5>
+          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+        </div>
+        <div class="col-md-3">
+          <div class="icon">
+            <i class="fas fa-hotel"></i>
+          </div>
+          <h5>Planning</h5>
+          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+        </div>
+        <div class="col-md-3">
+          <div class="icon">
+            <i class="fas fa-hotel"></i>
+          </div>
+          <h5>Planning</h5>
+          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+        </div>
+        <div class="col-md-3">
+          <div class="icon">
+            <i class="fas fa-hotel"></i>
+          </div>
+          <h5>Planning</h5>
+          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+</body>
+</html>
