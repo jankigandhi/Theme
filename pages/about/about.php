c112f06bcd346989f7e5e0888e1c76440cf5395b
Janki Gandhi <jankigandhi@atyantik.com>
2018-08-21 13:02:50 +0530

    designing about us page

--- pages/about/about.php
+++ pages/about/about.php
@@ -1,5 +1,7 @@
 <?php include_once '../../inc/head.php';?>
 <link rel="stylesheet" type="text/css" href="../../components/breadcrumbs/css/breadcrumbs.css">
+<link rel="stylesheet" type="text/css" href="../../components/progressbar/css/progressbar.css">
+<link rel="stylesheet" type="text/css" href="../../components/card/css/card.css">
 <link rel="stylesheet" href="css/about.css">
 </head>
 <body>
@@ -29,8 +31,8 @@
       </ul>
     </div>
   </div>
-  <div class="container">
-    <div class="row py-4">
+  <div class="container py-5">
+    <div class="row">
       <div class="col-md-7 py-2">
         <h3 class="text-uppercase">Lorem ipsum is simply</h3>
         <p class="light-gray">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -40,38 +42,243 @@
       </div>
     </div>
   </div>
-  <div class="work-section text-center py-5">
+  <div class="work-section py-5">
     <div class="container">
-      <h3 class="text-uppercase">How it Works</h3>
-      <p>Absolutely Stunning Design and Functionality</p>
-      <div class="row">
+      <h3 class="text-uppercase text-center">How it Works</h3>
+      <p class="light-gray text-center">Absolutely Stunning Design and Functionality</p>
+      <div class="row my-4">
         <div class="col-md-3">
-          <div class="icon">
-            <i class="fas fa-hotel"></i>
+          <div class="hexagon">
+            <span class="icon"><i class="fas fa-hotel"></i></span>
           </div>
-          <h5>Planning</h5>
-          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          <div class="text-center">
+            <h5>Planning</h5>
+            <p class="light-gray small px-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          </div>
         </div>
         <div class="col-md-3">
-          <div class="icon">
-            <i class="fas fa-hotel"></i>
+          <div class="hexagon">
+            <span class="icon"><i class="fas fa-camera-retro"></i></span>
           </div>
-          <h5>Planning</h5>
-          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          <div class="text-center">
+            <h5>Prototyping</h5>
+            <p class="light-gray small px-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          </div>
         </div>
         <div class="col-md-3">
-          <div class="icon">
-            <i class="fas fa-hotel"></i>
+          <div class="hexagon">
+            <span class="icon"><i class="fas fa-grin-hearts"></i></span>
           </div>
-          <h5>Planning</h5>
-          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          <div class="text-center">
+            <h5>Designing</h5>
+            <p class="light-gray small px-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          </div>
         </div>
         <div class="col-md-3">
-          <div class="icon">
-            <i class="fas fa-hotel"></i>
+          <div class="hexagon">
+            <span class="icon"><i class="fas fa-home"></i></span>
           </div>
-          <h5>Planning</h5>
-          <p class="light-gray small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          <div class="text-center">
+            <h5>Product Launch</h5>
+            <p class="light-gray small px-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.printing and typesetting industry.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-6 py-2">
+        <h4>Why Choose US?</h4>
+        <p class="light-gray">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        <ul class="row list-unstyled">
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Web and Graphic Design</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Mobile Game Design</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">CMS Integration</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Software Development</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Website Development</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Mobile App Development</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Mobile Games</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Software Development</span>
+            </p>
+          </li>
+          <li class="col-sm-6">
+            <p class="d-table light-gray">
+              <span class="d-table-cell"><i class="font-weight-bold pr-3 text-primary far fa-check-circle"></i></span>
+              <span class="d-table-cell">Mobile App Development</span>
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-6 py-2">
+        <h4>Why Skills</h4>
+        <ul class="list-unstyled">
+          <li class="mb-4">
+            <p class="mb-1 font-weight-medium light-gray">
+              CSS/HTML
+            </p>
+            <div class="progress-horizontal-bar d-inline-block w-100 mb-0">
+              <p class="bar bg-primary float-left py-2 mb-0" style="width: 90%;">
+                <span class="font-weight-medium light-gray">90%</span>
+              </p>
+            </div>
+          </li>
+          <li class="mb-4">
+            <p class="mb-1 font-weight-medium light-gray">
+              jQuery
+            </p>
+            <div class="progress-horizontal-bar d-inline-block w-100 mb-0">
+              <p class="bar bg-primary float-left py-2 mb-0" style="width: 85%;">
+                <span class="font-weight-medium light-gray">85%</span>
+              </p>
+            </div>
+          </li>
+          <li class="mb-4">
+            <p class="mb-1 font-weight-medium light-gray">
+              CMS Projects
+            </p>
+            <div class="progress-horizontal-bar d-inline-block w-100 mb-0">
+              <p class="bar bg-primary float-left py-2 mb-0" style="width: 70%;">
+                <span class="font-weight-medium light-gray">70%</span>
+              </p>
+            </div>
+          </li>
+          <li class="mb-4">
+            <p class="mb-1 font-weight-medium light-gray">
+              SEO
+            </p>
+            <div class="progress-horizontal-bar d-inline-block w-100 mb-0">
+              <p class="bar bg-primary float-left py-2 mb-0" style="width: 60%;">
+                <span class="font-weight-medium light-gray">60%</span>
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="team-section py-5">
+    <div class="container">
+      <h3 class="text-uppercase text-center">Meet the team</h3>
+      <p class="light-gray text-center">Absolutely Stunning Design and Functionality</p>
+      <div class="row my-4">
+        <div class="col-md-3">
+          <div class="team-box custom-card card">
+            <div class="bg-img" style="background-image: url('../../images/img-5.jpg');"></div>
+            <div class="team-content card-body p-2 text-center">
+              <h5 class="title text-uppercase mb-0">Your Title Here</h5>
+              <span class="post font-italic small light-gray">Co-Founder</span>
+              <div>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="team-box custom-card card">
+            <div class="bg-img" style="background-image: url('../../images/img-5.jpg');"></div>
+            <div class="team-content card-body p-2 text-center">
+              <h5 class="title text-uppercase mb-0">Your Title Here</h5>
+              <span class="post font-italic small light-gray">Co-Founder</span>
+              <div>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="team-box custom-card card">
+            <div class="bg-img" style="background-image: url('../../images/img-5.jpg');"></div>
+            <div class="team-content card-body p-2 text-center">
+              <h5 class="title text-uppercase mb-0">Your Title Here</h5>
+              <span class="post font-italic small light-gray">Co-Founder</span>
+              <div>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="team-box custom-card card">
+            <div class="bg-img" style="background-image: url('../../images/img-5.jpg');"></div>
+            <div class="team-content card-body p-2 text-center">
+              <h5 class="title text-uppercase mb-0">Your Title Here</h5>
+              <span class="post font-italic small light-gray">Co-Founder</span>
+              <div>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
