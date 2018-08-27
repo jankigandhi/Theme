c0f412350101c9ba8e7ede84de5bb405f74b2a94
Harsh Sharma <jankigandhi@atyantik.com>
2018-08-27 21:24:21 +0530

    design changes theme-home page

---
+++ pages/theme-home/home.php
@@ -0,0 +1,205 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../../components/img-gallery/css/gallery.css">
+<link rel="stylesheet" href="../../components/testimonial/css/testimonial.css">
+<link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+<?php $HederClass="";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+    <div class="theme-home-page">
+        <div class="banner-content">
+            <div class="theme-home" style="background-image: url('images/theme-home-banner.jpg');">
+                <div class="home-banner-text">
+                    <h3 class="home-title mb-0 text-white text-center">Foundry brings your content to life in stunning clarity</h3>
+                    <p class="sub-title text-uppercase text-white text-center mb-0 py-3 font-weight-medium">A Complete Block-Based Solution</p>
+                    <P class="banner-desc text-white text-center mx-auto mb-0">Build beautiful, contemporary sites in just moments with Foundry and Variant Page Builder.</P>
+                    <div class="text-center my-5">
+                        <button type="button" class="btn-thick btn-min-w rounded-0 btn btn-outline-white">Start Exploring</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="text-center my-5">
+                <h2 class="py-4 mb-0">Sleek, Flexible & Stylish.</h2>
+                <p class="cotent-desc mx-auto mb-0">Foundry is a remarkably complete template offering you a plethora of handcrafted design elements. Whether promoting a product, service or portfolio - Foundry's unique and flexible style has you covered.</p>
+            </div>
+            <div class="row mx-0 my-5 align-items-center">
+                <div class="col-lg-7 col-md-6">
+                    <img class="images mw-100" src="images/image.jpg" alt="Screenshort">
+                </div>
+                <div class="col-lg-4 col-md-5">
+                    <div class="pl-md-5 py-4 pt-md-0">
+                        <h4 class="pr-5">Build a slick, modern site faster than ever</h4>
+                        <p class="foundry-desc mb-0">Foundry is your complete design toolkit, built from the ground up to be flexible, extensible and stylish. Building slick, contemporary sites has never been this easy!</p>
+                        <div class="my-4">
+                            <button type="button" class="btn-thick btn-min-w rounded-0 btn btn-outline-primary text-uppercase">Explore Foundry</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="p-4">
+                <h4 class="text-center my4">Multi-Page Layouts</h4>
+                <div>
+                    <div class="button-group filters-button-group">
+                        <button class="button btn btn-link px-0 is-checked" data-filter="*">
+                        View All
+                        <span class="px-2">/</span>
+                        </button>
+                        <button class="button btn btn-link px-0" data-filter=".web-design">
+                        Web Design
+                        <span class="px-2">/</span>
+                        </button>
+                        <button class="button btn btn-link px-0" data-filter=".graphic-design">     Graphic Design
+                        <span class="px-2">/</span>
+                        </button>
+                        <button class="button btn btn-link px-0" data-filter=".website-templates">
+                        Website Templates
+                        </button>
+                    </div>
+                    <div class="grid row my-4 mx-0">
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item web-design">
+                            <a href="images/gallery-1.png" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-1.png');"></div>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item web-design">
+                            <a href="images/gallery-2.png" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-2.png');"></div>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item website-templates">
+                            <a href="images/gallery-3.jpg" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-3.jpg');"></div>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item graphic-design">
+                            <a href="images/gallery-1.png" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-1.png');"></div>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item web-design">
+                            <a href="images/gallery-2.png" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-2.png');"></div>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item website-templates">
+                            <a href="images/gallery-3.jpg" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-3.jpg');"></div>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item website-templates">
+                            <a href="images/gallery-1.png" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-1.png');"></div>
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 p-2 element-item graphic-design">
+                            <a href="images/gallery-2.png" class="img-block d-block glightbox">
+                                <div class="gallery-img hover-effect" style="background-image: url('images/gallery-2.png');"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="testimonial">
+        <div class="banner py-5" style="background-image: url('images/perellex.jpg');">
+            <div class="container">
+                <div class="testimonial py-5 d-inline-block w-100">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6 py-3">
+                            <div class="h5 text-white">
+                                <i class="quote-icon fas fa-quote-left align-super"></i>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.
+                            </div>
+                            <h6 class="text-white mb-0 text-right text-uppercase font-weight-bold">
+                            Janki
+                            <span class="d-block font-weight-bold font-italic">Web designer</span>
+                            </h6>
+                        </div>
+                        <div class="col-md-4 col-sm-6 py-3">
+                            <div class="h5 text-white">
+                                <i class="quote-icon fas fa-quote-left align-super"></i>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.
+                            </div>
+                            <h6 class="text-white mb-0 text-right text-uppercase font-weight-bold">
+                            Janki
+                            <span class="d-block font-weight-bold font-italic">Web designer</span>
+                            </h6>
+                        </div>
+                        <div class="col-md-4 col-sm-6 py-3">
+                            <div class="h5 text-white">
+                                <i class="quote-icon fas fa-quote-left align-super"></i>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.
+                            </div>
+                            <h6 class="text-white mb-0 text-right text-uppercase font-weight-bold">
+                            Janki
+                            <span class="d-block font-weight-bold font-italic">Web designer</span>
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+<script src="../../js/glightbox.min.js"></script>
+<script>
+var lightbox = GLightbox();
+</script>
+<!-- isotop -->
+<script src="../../js/isotope.pkgd.js"></script>
+<script>
+// init Isotope
+var iso = new Isotope( '.grid', {
+itemSelector: '.element-item',
+layoutMode: 'fitRows'
+});
+// filter functions
+var filterFns = {
+// show if number is greater than 50
+numberGreaterThan50: function( itemElem ) {
+var number = itemElem.querySelector('.number').textContent;
+return parseInt( number, 10 ) > 50;
+},
+// show if name ends with -ium
+ium: function( itemElem ) {
+var name = itemElem.querySelector('.name').textContent;
+return name.match( /ium$/ );
+}
+};
+// bind filter button click
+var filtersElem = document.querySelector('.filters-button-group');
+filtersElem.addEventListener( 'click', function( event ) {
+// only work with buttons
+if ( !matchesSelector( event.target, 'button' ) ) {
+return;
+}
+var filterValue = event.target.getAttribute('data-filter');
+// use matching filter function
+filterValue = filterFns[ filterValue ] || filterValue;
+iso.arrange({ filter: filterValue });
+});
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll('.button-group');
+for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+var buttonGroup = buttonGroups[i];
+radioButtonGroup( buttonGroup );
+}
+function radioButtonGroup( buttonGroup ) {
+buttonGroup.addEventListener( 'click', function( event ) {
+// only work with buttons
+if ( !matchesSelector( event.target, 'button' ) ) {
+return;
+}
+buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+event.target.classList.add('is-checked');
+});
+}
+</script>
+</body>
+</html>
