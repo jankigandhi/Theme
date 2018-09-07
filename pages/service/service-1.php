92723806dc626ad825c360c573bbee588cc3ba5a
Harsh Sharma <jankigandhi@atyantik.com>
2018-09-07 16:16:03 +0530

    services page design changes

--- pages/service/service-1.php
+++ pages/service/service-1.php
@@ -10,14 +10,260 @@
 <?php include_once '../../components/nav-bar/nav-bar.php';?>
 <main>
 	<div class="services-pages">
-		<div class="banner-img" style="background-image: url('images/services-banner-1.jpg');">
-			<div class="services-text">
+		<div class="banner-img" style="background-image: url('images/services-banner.jpg');">
+			<div class="services-contain">
 				<div class="container">
-					<p class="mb-0 text-white">We provide innovative solutions to expand business</p>
-					<h2 class="services-title mx-auto text-white mb-0 py-3">We have been helping build brands</h2>
+					<p class="text-white mb-0">We provide innovative solutions to expand business</p>
+					<h2 class="service-desc text-white mb-0 mx-auto py-4">We have been helping build brands</h2>
+					<div class="my-5">
+						<button type="button" class="btn-thick btn-min-w rounded-0 btn btn-outline-white">Start Folling</button>
+					</div>
 				</div>
 			</div>
-			
+		</div>
+		<div class="container">
+			<div class="services-contain my-5">
+				<h3 class="text-center font-weight-bold text-uppercase">Why Work With Us?</h3>
+				<div class="row mx-0">
+					<div class="col-md-4">
+						<div class="my-4">
+							<div class= "blog-images" style="background-image: url('images/blog-img.jpg');"></div>
+							<div class="blog-contain text-center">
+								<h5>We believe in creativit</h5>
+								<p class="light-gray">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="my-4">
+							<div class= "blog-images" style="background-image: url('images/blog-img-1.jpg');"></div>
+							<div class="blog-contain text-center">
+								<h5>We believe in creativit</h5>
+								<p class="light-gray">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="my-4">
+							<div class= "blog-images" style="background-image: url('images/blog-img-2.jpg');"></div>
+							<div class="blog-contain text-center">
+								<h5>We believe in creativit</h5>
+								<p class="light-gray">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="feacher-section bg-primary py-5">
+			<div class="container">
+				<div class="row mx-0">
+					<div class="col-lg-4 col-md-6 my-5">
+						<div class="text-center">
+							<span class="social-icon">
+								<i class="fas fa-desktop text-white"></i>
+							</span>
+							<div>
+								<h5 class="text-white mb-0 my-2">Web Development</h5>
+								<p class="feacher-desc mx-auto text-white mb-0 font-weight-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 my-5">
+						<div class="text-center">
+							<span class="social-icon">
+								<i class="fas fa-pencil-ruler text-white"></i>
+							</span>
+							<div>
+								<h5 class="text-white mb-0 my-2">Logo & Identity</h5>
+								<p class="feacher-desc mx-auto text-white mb-0 font-weight-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 my-5">
+						<div class="text-center">
+							<span class="social-icon">
+								<i class="fas fa-anchor text-white"></i>
+							</span>
+							<div>
+								<h5 class="text-white mb-0 my-2">Graphics Design</h5>
+								<p class="feacher-desc mx-auto text-white mb-0 font-weight-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 my-5">
+						<div class="text-center">
+							<span class="social-icon">
+								<i class="fas fa-arrows-alt text-white"></i>
+							</span>
+							<div>
+								<h5 class="text-white mb-0 my-2">App Development</h5>
+								<p class="feacher-desc mx-auto text-white mb-0 font-weight-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 my-5">
+						<div class="text-center">
+							<span class="social-icon">
+								<i class="fas fa-cut text-white"></i>
+							</span>
+							<div>
+								<h5 class="text-white mb-0 my-2">Social Marketing</h5>
+								<p class="feacher-desc mx-auto text-white mb-0 font-weight-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 my-5">
+						<div class="text-center">
+							<span class="social-icon">
+								<i class="fas fa-file-contract text-white"></i>
+							</span>
+							<div>
+								<h5 class="text-white mb-0 my-2">Content Creation</h5>
+								<p class="feacher-desc mx-auto text-white mb-0 font-weight-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="pricing-table py-5 text-center">
+			<div class="container">
+				<h3 class="text-uppercase">Pricing Table</h3>
+				<p class="light-gray">Absolutely Stunning Design and Functionality</p>
+				<div class="row">
+					<div class="col-lg-3 col-sm-6 py-4">
+						<div class="card">
+							<div class="card-body pricing-box">
+								<h4 class="price-title">Standard</h4>
+								<div class="circle-icon rounded-circle d-flex justify-content-center align-items-center mx-auto my-4">
+									<div class="circle-text">
+										<p class="mb-0 small">$
+											<span class="number align-top">29</span>
+										</p>
+										<p class="small mb-0">/month</p>
+									</div>
+								</div>
+								<ul class="list-unstyled">
+									<li class="price-list my-2">15 Projects</li>
+									<li class="price-list my-2">2 Email Marketing</li>
+									<li class="price-list my-2">5 Data Base</li>
+									<li class="price-list my-2">30 GB Stroge</li>
+									<li class="price-list my-2">Unlimited Data Transfer</li>
+									<li class="price-list my-2">50 GB Bandwith</li>
+									<li class="price-list my-2">Enhanced Security</li>
+								</ul>
+								<div class="my-4">
+									<button class="btn-hover rounded-0 btn btn-outline-primary font-weight-medium">Get Started Now</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 py-4">
+						<div class="card">
+							<div class="card-body pricing-box">
+								<h4 class="price-title">Business</h4>
+								<div class="circle-icon rounded-circle d-flex justify-content-center align-items-center mx-auto my-4">
+									<div class="circle-text">
+										<p class="mb-0 small">$
+											<span class="number align-top">39</span>
+										</p>
+										<p class="small mb-0">/month</p>
+									</div>
+								</div>
+								<ul class="list-unstyled">
+									<li class="price-list my-2">15 Projects</li>
+									<li class="price-list my-2">2 Email Marketing</li>
+									<li class="price-list my-2">5 Data Base</li>
+									<li class="price-list my-2">30 GB Stroge</li>
+									<li class="price-list my-2">Unlimited Data Transfer</li>
+									<li class="price-list my-2">50 GB Bandwith</li>
+									<li class="price-list my-2">Enhanced Security</li>
+								</ul>
+								<div class="my-4">
+									<button class="btn-hover rounded-0 btn btn-outline-primary font-weight-medium">Get Started Now</button>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 py-4">
+						<div class="card">
+							<div class="card-body pricing-box">
+								<h4 class="price-title">Premium</h4>
+								<div class="circle-icon rounded-circle d-flex justify-content-center align-items-center mx-auto my-4">
+									<div class="circle-text">
+										<p class="mb-0 small">$
+											<span class="number align-top">49</span>
+										</p>
+										<p class="small mb-0">/month</p>
+									</div>
+								</div>
+								<ul class="list-unstyled">
+									<li class="price-list my-2">15 Projects</li>
+									<li class="price-list my-2">2 Email Marketing</li>
+									<li class="price-list my-2">5 Data Base</li>
+									<li class="price-list my-2">30 GB Stroge</li>
+									<li class="price-list my-2">Unlimited Data Transfer</li>
+									<li class="price-list my-2">50 GB Bandwith</li>
+									<li class="price-list my-2">Enhanced Security</li>
+								</ul>
+								<div class="my-4">
+									<button class="btn-hover rounded-0 btn btn-outline-primary font-weight-medium">Get Started Now</button>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 py-4">
+						<div class="card">
+							<div class="card-body pricing-box">
+								<h4 class="price-title">Ultimate</h4>
+								<div class="circle-icon rounded-circle d-flex justify-content-center align-items-center mx-auto my-4">
+									<div class="circle-text">
+										<p class="mb-0 small">$
+											<span class="number align-top">59</span>
+										</p>
+										<p class="small mb-0">/month</p>
+									</div>
+								</div>
+								<ul class="list-unstyled">
+									<li class="price-list my-2">15 Projects</li>
+									<li class="price-list my-2">2 Email Marketing</li>
+									<li class="price-list my-2">5 Data Base</li>
+									<li class="price-list my-2">30 GB Stroge</li>
+									<li class="price-list my-2">Unlimited Data Transfer</li>
+									<li class="price-list my-2">50 GB Bandwith</li>
+									<li class="price-list my-2">Enhanced Security</li>
+								</ul>
+								<div class="my-4">
+									<button class="btn-hover rounded-0 btn btn-outline-primary font-weight-medium">Get Started Now</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="parallax-banner banner">
+				<div class="banner-img" style="background-image: url('images/services-banner-1.jpg');">
+					<div class="banner-content">
+						<div class="center-content">
+							<div class="container">
+								<span class="play-icon">
+									<i class="far fa-play-circle text-white"></i>
+								</span>
+								<h3 class="text-white py-4 font-weight-bold">Find More Creative Ideas For Your Projects</h3>
+								<p class="parallax-desc text-white mx-auto font-weight-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+								<div class="my-5">
+									<button type="button" class="btn-thick btn-min-w rounded-0 btn btn-outline-white">More Ideas</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
