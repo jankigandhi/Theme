90a2b047e615a92a19a084ad58e918fd2bad1838
Harsh Sharma <jankigandhi@atyantik.com>
2018-09-07 20:20:50 +0530

    about page design changes

---
+++ pages/about/about-1.php
@@ -0,0 +1,96 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" type="text/css" href="../../components/breadcrumbs/css/breadcrumbs.css">
+<link rel="stylesheet" type="text/css" href="../../components/progressbar/css/progressbar.css">
+<link rel="stylesheet" type="text/css" href="../../components/card/css/card.css">
+<link rel="stylesheet" href="css/about.css">
+</head>
+<body>
+<?php $HederClass="header-transparent";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+    <div class="about-pages">
+        <div class="banner">
+            <div class="banner-img" style="background-image: url('images/banner-img.jpg');"></div>
+            <div class="banner-content">
+                <div class="center-content">
+                    <div class="container">
+                        <h3 class="about-title text-white mx-auto">We are not your average brand agency</h3>
+                        <p class="text-white about-desc mx-auto py-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since. Lorem Ipsum has been the industry’s standard dummy text ever since. Lorem Ipsum is printing and typesetting simply dummy text.</p>
+                        <div>
+                            <button type="button" class="btn-thick btn-min-w rounded-0 btn btn-outline-white">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="project-info">
+            <div class="container">
+                <div class="row mx-0 my-5">
+                    <div class="col-lg-3 col-md-6 project-content py-4">
+                        <div class="text-center">
+                            <span class="icon-social">
+                                <i class="far fa-heart text-primary"></i>
+                            </span>
+                            <div>
+                                <span class="number font-weight-bold my-3">4560</span>
+                                <p class="light-gray">Happy Clients</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 project-content py-4">
+                        <div class="text-center">
+                            <span class="icon-social">
+                               <i class="fas fa-tv text-primary"></i>
+                            </span>
+                            <div>
+                                <span class="number font-weight-bold my-3">3790</span>
+                                <p class="light-gray">Project Completed</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 project-content py-4">
+                        <div class="text-center">
+                            <span class="icon-social">
+                               <i class="fas fa-camera text-primary"></i>
+                            </span>
+                            <div>
+                                <span class="number font-weight-bold my-3">5580</span>
+                                <p class="light-gray">Photo Capture</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 project-content py-4">
+                        <div class="text-center">
+                            <span class="icon-social">
+                               <i class="fas fa-comments text-primary"></i>
+                            </span>
+                            <div>
+                                <span class="number font-weight-bold my-3">8580</span>
+                                <p class="light-gray">Telephonic Talk</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="reason-work">
+            <div class="row mx-0 my-5">
+                <div class="col-md-6 px-0 ">
+                    <div class="about-img" style="background-image: url('images/about-img.jpg');"></div>
+                </div>
+                <div class="col-md-6 px-0 reason-content">
+                    <h3 class="text-center py-5 text-uppercase">Some Reasons To Work Together</h3>
+                    <div class="row mx-0">
+                        <div class="col-md-6 px-4">
+                            <h5><span class="pr-3 text-primary">01.</span> We believe in creativity</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+</body>
+</html>
