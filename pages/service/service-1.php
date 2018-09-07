45394f3134b6115a0db3b023b63cd66387d8d1e7
Harsh Sharma <jankigandhi@atyantik.com>
2018-09-07 10:21:00 +0530

    services page design changes

---
+++ pages/service/service-1.php
@@ -0,0 +1,27 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" type="text/css" href="../../components/breadcrumbs/css/breadcrumbs.css">
+<link rel="stylesheet" type="text/css" href="../../components/accordions/css/accordions.css">
+<link rel="stylesheet" type="text/css" href="../../components/progressbar/css/progressbar.css">
+<link rel="stylesheet" type="text/css" href="../../components/card/css/card.css">
+<link rel="stylesheet" href="css/service.css">
+</head>
+<body>
+<?php $HederClass="header-transparent";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+	<div class="services-pages">
+		<div class="banner-img" style="background-image: url('images/services-banner-1.jpg');">
+			<div class="services-text">
+				<div class="container">
+					<p class="mb-0 text-white">We provide innovative solutions to expand business</p>
+					<h2 class="services-title mx-auto text-white mb-0 py-3">We have been helping build brands</h2>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+</body>
+</html>
