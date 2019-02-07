9327ec25cc49d702727226474139a15fbecb0215
Harsh Sharma <jankigandhi@atyantik.com>
2019-02-07 12:34:03 +0530

    blog page design changes

---
+++ pages/blog/blog.php
@@ -0,0 +1,16 @@
+<?php include_once '../../inc/head.php';?>
+<link rel="stylesheet" href="../../css/libs/swiper.min.css">
+<link rel="stylesheet" href="../../components/card/css/card.css">
+<link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+<?php $HederClass="fixed-header";?>
+<?php include_once '../../components/nav-bar/nav-bar.php';?>
+<main>
+   
+</main>
+<?php include_once '../../components/footer/footer.php';?>
+<?php include_once '../../inc/foot.php';?>
+
+</body>
+</html>
