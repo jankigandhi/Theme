ba92f129b1ea8c5bc287264e00b78836eeca5d3c
Harsh Sharma <jankigandhi@atyantik.com>
2019-02-12 11:29:20 +0530

    design changes blog page

--- pages/blog/blog.php
+++ pages/blog/blog.php
@@ -1,13 +1,32 @@
 <?php include_once '../../inc/head.php';?>
 <link rel="stylesheet" href="../../css/libs/swiper.min.css">
 <link rel="stylesheet" href="../../components/card/css/card.css">
-<link rel="stylesheet" href="css/home.css">
+<link rel="stylesheet" href="css/blog.css">
 </head>
 <body>
-<?php $HederClass="fixed-header";?>
+<?php $HederClass="header-transparent";?>
 <?php include_once '../../components/nav-bar/nav-bar.php';?>
 <main>
-   
+   <div class="blog-page">
+   		<div class="banner-img blog-img" style="background-image: url('images/banner-blog.jpg');">
+   			<div class="blog-content text-center">
+   				<h3 class="text-white blog-title font-weight-bold">Clean Blog</h3>
+   				<p class="text-white blog-desc">A Blog Theme by Ultimate</p>
+   			</div>
+   		</div>
+   		<div class="blog-post py-5">
+	   		<div class="container">
+	   			<div class="blog-div mx-auto">
+	   				<a href="#">
+	   					<h2>Man must explore, and this is exploration at its greatest</h2>
+	   					<p class="blog-desc">Problems look mighty small from 150 miles up</p>
+	   					<div class="blog-date">Posted by Start Bootstrap on September 24, 2019</div>		
+	   				</a>
+	   			</div>
+	   		</div>
+   		</div>
+   </div>
+  
 </main>
 <?php include_once '../../components/footer/footer.php';?>
 <?php include_once '../../inc/foot.php';?>
